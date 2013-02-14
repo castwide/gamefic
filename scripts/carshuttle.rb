1e10eb8914a4feaec05290c9b653e7104fd77913
Fred Snyder <fred@blindacre.com>
2013-02-14 05:16:20 -0500

    Common scripts.

---
+++ scripts/carshuttle.rb
@@ -0,0 +1,46 @@
+require "lib/carshuttle.rb"
+
+action :drive, PARENT.reduce(Car), STRING do |actor, string|
+	actor.tell "Where do you want to go?"
+	passthru
+end
+
+action :drive, PARENT.reduce(Car) do |actor, car|
+	actor.tell "Available destinations:"
+	actor.story.entities.that_are(Waypoint).each { |waypoint|
+		actor.tell "#{waypoint.location}"
+	}
+end
+
+action :drive, PARENT.reduce(Car), ANYWHERE.reduce(Waypoint) do |actor, car, waypoint|
+	if actor.parent.parent != waypoint
+		actor.parent.parent = waypoint
+		actor.tell "You drive to #{waypoint.location}."
+	else
+		actor.tell "You're already there."
+	end
+end
+
+action :enter, PROXIMATE.reduce(Car) do |actor, car|
+	actor.parent = car
+	actor.tell "You get in #{car.longname}."
+end
+
+action :exit, PARENT.reduce(Car) do |actor, car|
+	actor.parent = car.parent
+	actor.tell "You get out of #{car.longname}."
+	actor.tell actor.parent.longname.upcase
+	actor.perform "itemize room"
+end
+
+instruct("drive", :drive, "car")
+instruct("drive [place]", :drive, "car [place]")
+instruct("drive to [place]", :drive, "car [place]")
+instruct("go to [place]", :drive, "car [place]")
+instruct("drive [car] to [place]", :drive, "[car] [place]")
+
+instruct("get out [place]", :exit, "[place]")
+instruct("get out", :exit, "car")
+instruct("get in [place]", :enter, "[place]")
+instruct("exit", :exit, "car")
+instruct("out", :exit, "car")
---
+++ scripts/traversal.rb
@@ -0,0 +1,39 @@
+require "core/entities/room.rb"
+require "core/entities/portal.rb"
+
+action :go, NEARBY.reduce(Portal) do |actor, portal|
+	actor.parent = portal.destination
+	actor.tell "You go #{portal.name}."
+	actor.tell actor.parent.longname.upcase
+	actor.perform "itemize room"
+end
+
+action :go, STRING do |actor, string|
+	actor.tell "You don't see any exit \"#{string}\" from here."
+end
+
+action :go do |actor|
+	actor.tell "Where do you want to go?"
+end
+
+instruct "north", :go, "north"
+instruct "south", :go, "south"
+instruct "west", :go, "west"
+instruct "east", :go, "east"
+instruct "up", :go, "up"
+instruct "down", :go, "down"
+instruct "northwest", :go, "northwest"
+instruct "northeast", :go, "northeast"
+instruct "southwest", :go, "southwest"
+instruct "southeast", :go, "southeast"
+
+instruct "n", :go, "north"
+instruct "s", :go, "south"
+instruct "w", :go, "west"
+instruct "e", :go, "east"
+instruct "u", :go, "up"
+instruct "d", :go, "down"
+instruct "nw", :go, "northwest"
+instruct "ne", :go, "northeast"
+instruct "sw", :go, "southwest"
+instruct "se", :go, "southeast"
