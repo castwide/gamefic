1abf6b784dd5ec940b7112e0f286a8d60ae44386
Fred Snyder <fsnyder@castwide.com>
2013-02-21 12:45:36 -0500

    Array and String helper functions.

---
+++ lib/array.rb
@@ -0,0 +1,24 @@
+class Array
+	def that_are(cls)
+		return self.clone.delete_if { |i| i.kind_of?(cls) == false }
+	end
+	def that_are_not(cls)
+		return self.clone.delete_if { |i| i.kind_of?(cls) == true }
+	end
+	def random
+		return self[rand(self.length)]
+	end
+	def shuffle
+		self.clone.shuffle
+	end
+	def shuffle!
+		self.sort! { |a, b|
+			rand(3) <=> rand(3)
+		}
+	end
+end
+
+x = ['a','b','c','d','e']
+puts "#{x}"
+x.shuffle!
+puts "#{x}"
