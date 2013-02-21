b6f81172c0acd57ca2b06aa758ddd97d8c8dbddd
Fred Snyder <fsnyder@castwide.com>
2013-02-21 12:36:52 -0500

    Generic Zone class for grouping entities.

--- lib/zone.rb
+++ lib/zone.rb
@@ -3,13 +3,20 @@ require "lib/describable"
 
 module Gamefic
 
+	# An optional class for grouping entities.
 	class Zone
 		include Branch
-		include Describable
-		def initialize(args = {})
-			args.each { |key, value|
-				send "#{key}=", value
-			}
+		# TODO: In all likelihood, Zones don't need
+		# properties. They just need the capabilities
+		# of a node to provide easy transportability.
+		#include Describable
+		#def initialize(args = {})
+		#	args.each { |key, value|
+		#		send "#{key}=", value
+		#	}
+		#end
+		def update
+			# TODO: This is just a stub to keep story updates from failing.
 		end
 	end
 
