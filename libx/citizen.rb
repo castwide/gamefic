0a16e8bb1e619418e36d2f414de10acc4f1d1d30
Fred Snyder <fred@blindacre.com>
2013-02-25 16:33:06 -0500

    Citizen and Fugitive entities.

--- libx/citizen.rb
+++ libx/citizen.rb
@@ -1,15 +1,25 @@
+require "libx/personified"
+
 module Gamefic
 
 	class Citizen < Character
-		@@randchar = RandomCharacters.new
+		include Personified
 		def post_initialize
-			name = @@randchar.generate
+			super
+			personify
+			@synonyms = proper_name.full
 			on_update do |char|
-				if rand(10) == 1
+				if rand(20) == 1
 					portals = char.parent.children.that_are(Portal)
 					char.perform "go #{portals.random.name}"
 				end
 			end
+		end
+		def name
+			"#{physique} #{gender_noun}"
+		end
+		def name
+			"a #{physique} #{gender_noun}"
 		end
 	end
 	class Fugitive < Citizen
