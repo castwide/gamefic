347f7731957c89d33de72f7cd4d251cd33b38713
Fred Snyder <fsnyder@castwide.com>
2013-04-20 15:02:39 -0400

    Removed scripts. Most files are either deprecated or moved to examples.

--- scripts/carshuttle.rb
+++
@@ -1,5 +0,0 @@
-require "libx/carshuttle.rb"
-
-action :drive do |actor|
-	actor.state = Character::Drive
-end
--- scripts/speech.rb
+++
@@ -1,3 +0,0 @@
-action :say, query(:string) do |actor, message|
-	actor.parent.tell "#{actor.longname.cap_first} says, \"#{message}\""
-end
