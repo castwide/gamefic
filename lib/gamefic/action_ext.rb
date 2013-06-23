cfb2ac62dc2e2ad8dbfe91e171bf0c92b7007f5a
Fred Snyder <fsnyder@castwide.com>
2013-06-22 22:16:14 -0400

    Removed unnecessary ext scripts (replaced by require_defaults).

--- lib/gamefic/action_ext.rb
+++
@@ -1,6 +0,0 @@
-# Load a set of commands for moving around rooms, carrying items, looking at
-# things, and other basic interactivity.
-
-Dir[File.dirname(__FILE__) + '/action_ext/*.rb'].each do |file|
-	require file
-end
