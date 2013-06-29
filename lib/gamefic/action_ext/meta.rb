d7435c3e4b0ad840a9c763cd4aa0824a45cc409d
Fred Snyder <fsnyder@castwide.com>
2013-06-29 16:26:59 -0400

    Default meta commands (e.g., "commands" and "quit").

--- lib/gamefic/action_ext/meta.rb
+++ lib/gamefic/action_ext/meta.rb
@@ -1,7 +1,10 @@
 module Gamefic
 
 	Action.new nil, :quit do |actor|
-		exit
+		actor.destroy
+	end
+	Action.new nil, :commands do |actor|
+		actor.tell actor.plot.commandwords.sort.join(", ")
 	end
 
 end
