0378a75177db89ca8357aa9f6f5ae991ea58edbc
Fred Snyder <fsnyder@castwide.com>
2014-03-14 22:48:20 -0400

    Container entity supports closeability.

--- lib/gamefic/entity_ext/container.rb
+++ lib/gamefic/entity_ext/container.rb
@@ -4,6 +4,25 @@ module Gamefic
 
 	class Container < Entity
 		include Itemized
+    def closeable=(bool)
+      @closeable = bool
+    end
+    def closeable?
+      @closeable ||= false
+      if (@closeable == true && @closed == nil)
+        @closed = false
+      end
+      @closeable
+    end
+    def closed?
+      (@closed == true)
+    end
+    def closed=(bool)
+      if bool == true
+        @closeable = true
+      end
+      @closed = bool
+    end
 	end
 
 end
