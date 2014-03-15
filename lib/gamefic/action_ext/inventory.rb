28e57354b764d56d920e55dc832314ca205e5950
Fred Snyder <fsnyder@castwide.com>
2014-03-14 22:47:38 -0400

    Closeable containers. Smarter take action.

--- lib/gamefic/action_ext/inventory.rb
+++ lib/gamefic/action_ext/inventory.rb
@@ -11,20 +11,36 @@ module Gamefic
 
 	Action.new nil, :take, Query.new(:siblings, Portable) do |actor, thing|
 		thing.parent = actor
-		actor.tell "You take #{thing.longname}.", true
+		actor.tell "You take #{thing.longname.specify}.", true
 	end
 
 	Action.new nil, :take, Query.new(:siblings) do |actor, thing|
-		actor.tell "You can't carry #{thing.longname}."
+		actor.tell "You can't carry #{thing.longname.specify}."
 	end
 
-	Action.new nil, :take, String do |actor, thing|
-		actor.tell "You don't see anything called \"#{thing}\" here."
+	Action.new nil, :take, Query.new(:string) do |actor, thing|
+    containers = actor.children.that_are(Container)
+    containers = containers + actor.parent.children.that_are(Container)
+    found = false
+    containers.each { |container|
+      if container.closed? == false
+        query = Query.new(:children, Portable)
+        result = query.execute(container, thing)
+        if result.objects.length == 1
+          found = true
+          actor.perform "take #{result.objects[0].longname} from #{container.longname}"
+          break
+        end
+      end
+    }
+    if found == false
+      actor.tell "You don't see anything called \"#{thing}\" here."
+    end
 	end
 
 	Action.new nil, :drop, Query.new(:children) do |actor, thing|
 		thing.parent = actor.parent
-		actor.tell "You drop #{thing.longname}.", true
+		actor.tell "You drop #{thing.longname.specify}.", true
 	end
 
 	Syntax.new nil, "get :thing", :take, :thing
