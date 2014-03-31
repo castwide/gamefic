274c9baaab752a6a477901f6fc5dd427b9d34e32
Fred Snyder <fsnyder@castwide.com>
2014-03-31 15:12:37 -0400

    Experimental TerminalThread class for detecting client screen sizes.

--- lib/gamefic/engine/tty.rb
+++ lib/gamefic/engine/tty.rb
@@ -4,9 +4,67 @@ require 'thread'
 module Gamefic
 
   module Tty
+    class TerminalThread
+      # original detect_terminal_size method by cldwalker (https://github.com/cldwalker) (https://github.com/cldwalker/hirb/blob/master/lib/hirb/util.rb)
+      # modified to return an array of nil values instead of nil
+      def detect_terminal_size
+        if (ENV['COLUMNS'] =~ /^\d+$/) && (ENV['LINES'] =~ /^\d+$/)
+          [ENV['COLUMNS'].to_i, ENV['LINES'].to_i]
+        elsif (RUBY_PLATFORM =~ /java/ || (!STDIN.tty? && ENV['TERM']))
+          [`tput cols`.to_i, `tput lines`.to_i]
+        elsif STDIN.tty?
+          `stty size`.scan(/\d+/).map { |s| s.to_i }.reverse
+        else
+          [nil,nil]
+        end
+      rescue
+        [nil,nil]
+      end
+      def send(text)
+        @semaphore.synchronize {
+          @output.push [:text, text.to_s]
+        }      
+      end
+      def initialize
+        @output = Array.new
+        @semaphore = Mutex.new
+        @thread = Thread.new {
+          while true
+            @semaphore.synchronize {
+              @size = detect_terminal_size
+              while @output.length > 0
+                data = @output.shift
+                case data[0]
+                  when :html
+                    # TODO: Support HTML and/or Markdown
+                    print data[1]
+                  else
+                    print data[1]
+                end
+              end
+            }
+          end
+        }
+      end
+      def size
+        result = nil
+        @semaphore.synchronize {
+          result = @size
+        }
+        result
+      end
+      def running?
+        (@thread.stop? == false)
+      end
+    end
+    @@terminal_thread = TerminalThread.new
+    def self.terminal_thread
+      @@terminal_thread
+    end
     class Engine < Gamefic::Engine
       def post_initialize
         @user = Tty::User.new @plot
+        Tty::terminal_thread.send "\n"
       end
     end
     class User < Gamefic::User
@@ -19,16 +77,19 @@ module Gamefic
       def initialize
         super
       end
-      def send(data)
-        print "#{terminalize data}\n\n"
+      def size
+        Tty::terminal_thread.size
+      end
+      def send data
+        Tty::terminal_thread.send("#{terminalize(data, size[0])}\n\n")
       end
       def select(prompt)
-        print "#{terminalize prompt}"
+        Tty::terminal_thread.send "#{terminalize(prompt, size[0])}"
         line = STDIN.gets
-        send "\n"
+        Tty::terminal_thread.send "\n"
         @queue.push line.strip
       end
-      def terminalize string, max_length = 80
+      def terminalize string, max_length
         if max_length == nil
           return string
         end
