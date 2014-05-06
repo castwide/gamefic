e14ce1f83f9690e35a0e92027b0f1c76d527869d
Fred Snyder <fsnyder@castwide.com>
2014-05-05 22:12:29 -0400

    Drop action for items that are not in the player's inventory.

--- lib/gamefic/import/basics/actions/drop.rb
+++ lib/gamefic/import/basics/actions/drop.rb
@@ -1,3 +1,7 @@
+respond :drop, Query::Visible.new(Thing) do |actor, thing|
+  actor.tell "You're not carrying #{the thing}."
+end
+
 respond :drop, Query::Children.new() do |actor, thing|
   thing.parent = actor.parent
   actor.tell "You drop #{the thing}.", true
