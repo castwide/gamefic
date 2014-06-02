04fe493b1af715593d04fdd29c5c8aced9520fd7
Fred Snyder <fsnyder@castwide.com>
2014-06-01 20:28:53 -0400

    Retain object of pronoun in most cases. The object of the pronoun only gets cleared if the object is the character's parent or the action that was performed did not have any arguments.

--- lib/gamefic/director.rb
+++ lib/gamefic/director.rb
@@ -152,10 +152,11 @@ module Gamefic
 					opt = options.shift
 					if opt[1].length == 1
 						opt[0].execute(opt[1][0])
+            opt[1][0].object_of_pronoun = nil
 					else
             if opt[1].length == 2 and opt[1][1].kind_of?(Entity) and opt[1][0].parent != opt[1][1]
               opt[1][0].object_of_pronoun = opt[1][1]
-            else
+            elsif opt[1][0].parent == opt[1][1]
               opt[1][0].object_of_pronoun = nil
             end
 						opt[0].execute(opt[1])
@@ -178,10 +179,11 @@ module Gamefic
 						opt = @@delegation_stack.last.shift
 						if opt[1].length == 1
 							opt[0].execute(opt[1][0])
+              opt[1][0].object_of_pronoun = nil
 						else
               if opt[1].length == 2 and opt[1][1].kind_of?(Entity) and opt[1][0].parent != opt[1][1]
                 opt[1][0].object_of_pronoun = opt[1][1]
-              else
+              elsif opt[1][0].parent == opt[1][1]
                 opt[1][0].object_of_pronoun = nil
               end
 							opt[0].execute(opt[1])
