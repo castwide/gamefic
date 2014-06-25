d12aebfee4772ea7888a1165bc82727661769866
Fred Snyder <fsnyder@castwide.com>
2014-06-25 17:47:03 -0400

    Renamed "basics" to "standard."

--- lib/gamefic/import/basics.rb
+++
@@ -1,6 +0,0 @@
-import 'basics/entities/entity'
-import 'basics/entities/thing'
-import 'basics/entities/*'
-import 'basics/queries/*'
-import 'basics/actions/*'
-import 'basics/rules/*'
--- lib/gamefic/import/standard.rb
+++ lib/gamefic/import/standard.rb
@@ -1,1 +1,6 @@
-import 'basics'
+import 'standard/entities/entity'
+import 'standard/entities/thing'
+import 'standard/entities/*'
+import 'standard/queries/*'
+import 'standard/actions/*'
+import 'standard/rules/*'
