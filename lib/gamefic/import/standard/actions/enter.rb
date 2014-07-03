62f80895b2451739758d7d97fdc9df14aa0ed8c5
Fred Snyder <fsnyder@castwide.com>
2014-07-03 17:13:34 -0400

    Sit action is a shortcut for entering an enterable supporter.

--- lib/gamefic/import/standard/actions/enter.rb
+++ lib/gamefic/import/standard/actions/enter.rb
@@ -9,8 +9,21 @@ end
 respond :enter, Query::Siblings.new(Thing) do |actor, thing|
   actor.tell "#{The thing} can't accommodate you."
 end
+# Sit is a shortcut for entering an enterable supporter.
+respond :sit do |actor|
+  supporters = actor.room.children.that_are(Supporter).that_are(:enterable)
+  if supporters.length == 1
+    actor.perform "enter #{supporters[0]}"
+  elsif supporters.length > 1
+    actor.tell "I don't know where you want to sit: #{supporters.join_and(', ', ' or ')}."
+  else
+    actor.tell "There's nowhere to sit here."
+  end
+end
+
 xlate "sit :thing", :enter, :thing
 xlate "sit on :thing", :enter, :thing
 xlate "get on :thing", :enter, :thing
 xlate "get in :thing", :enter, :thing
 xlate "stand on :thing", :enter, :thing
+xlate "sit down", :sit
