c4095c0c7b5fb51c68589eabe5ada13bbac64484
Fred Snyder <fsnyder@castwide.com>
2014-07-03 17:16:23 -0400

    Containers are not portable by default.

--- lib/gamefic/import/standard/entities/container.rb
+++ lib/gamefic/import/standard/entities/container.rb
@@ -6,3 +6,4 @@ options(Container, :not_enterable, :enterable)
 options(Container, :opaque, :transparent)
 options(Container, :open, :closed).default = :open
 options(Container, :openable, :unopenable).default = :unopenable
+set_default_for(Container, :not_portable)
