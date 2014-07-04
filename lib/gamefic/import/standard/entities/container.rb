d6b86f2b173a3e83f4284a49488c6ee18102c2c1
Fred Snyder <fsnyder@castwide.com>
2014-07-04 17:15:53 -0400

    Lockable container options.

--- lib/gamefic/import/standard/entities/container.rb
+++ lib/gamefic/import/standard/entities/container.rb
@@ -1,9 +1,11 @@
 class Container < Thing
-
+  attr_accessor :unlocker
 end
 
 options(Container, :not_enterable, :enterable)
 options(Container, :opaque, :transparent)
 options(Container, :open, :closed).default = :open
 options(Container, :openable, :unopenable).default = :unopenable
+options(Container, :unlocked, :locked)
+options(Container, :not_lockable, :lockable)
 set_default_for(Container, :not_portable)
