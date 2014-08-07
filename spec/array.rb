8319e08e1ae52ad57654a03e9b1b6d59ce4e0379
Fred Snyder <fsnyder@castwide.com>
2014-08-07 02:26:26 -0400

    More Array specs.

--- spec/array.rb
+++ spec/array.rb
@@ -6,12 +6,21 @@ describe Array do
 		array = [0, 1, "two", 3]
 		filtered = array.that_are(String)
 		filtered.length.should eq(1)
+    filtered[0].should eq("two")
+	end
+  it "excludes by class" do
+		array = [0, 1, "two", 3]
 		filtered = array.that_are_not(String)
 		filtered.length.should eq(3)
-	end
+    filtered.include?("two").should eq(false)  
+  end
 	it "joins with a conjunction" do
 		array = ["one", "two", "three"]
 		array.join_and.should eq("one, two, and three")
 		array.join_and(', ', ' or ').should eq("one, two, or three")
 	end
+  it "joins two elements with the \"and\" separator" do
+    array = ["one", "two"]
+    array.join_and.should eq("one and two")
+  end
 end
