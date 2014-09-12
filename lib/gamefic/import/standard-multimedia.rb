9ef1e689217d681f1276d9a9baf5380e1b082fbb
Fred Snyder <fsnyder@castwide.com>
2014-09-11 22:20:58 -0400

    Standard multimedia rule to send room image to player when available.

--- lib/gamefic/import/standard-multimedia.rb
+++ lib/gamefic/import/standard-multimedia.rb
@@ -1,5 +1,8 @@
 class Entity
   attr_accessor :image
+  def has_image?
+    (@image.to_s != '')
+  end
 end
 
 class Character
@@ -12,4 +15,11 @@ class Character
   def play_ambient(filename, loop = false)
     # TODO: Implement
   end
+end
+
+assert_action :room_has_image do |actor, action|
+  if actor.room.has_image? and actor.room.is?(:lighted)
+    actor.show_image actor.room.image
+  end
+  true
 end
