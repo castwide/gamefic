8edc5e0956e2f169f28c0a609e3bbf87e78773ed
Fred Snyder <fsnyder@castwide.com>
2014-09-12 09:31:13 -0400

    Improved messages for invalid drop_in and drop_on actions.

--- lib/gamefic/import/standard/actions/drop-on.rb
+++ lib/gamefic/import/standard/actions/drop-on.rb
@@ -2,10 +2,31 @@ respond :drop_on, Query::Children.new(), Query::Reachable.new() do |actor, thing
   actor.tell "You can't put #{the thing} on #{the supporter}."
 end
 
+respond :drop_on, Query::Visible.new(), Query::Reachable.new(Supporter) do |actor, thing, supporter|
+  if thing.parent != actor
+    actor.perform :take, thing
+  end
+  if thing.parent == actor
+    actor.perform :drop_on, thing
+  end
+end
+
 respond :drop_on, Query::Children.new(), Query::Reachable.new(Supporter) do |actor, thing, supporter|
   thing.parent = supporter
   thing.is :supported
   actor.tell "You put #{the thing} on #{the supporter}."
+end
+
+respond :drop_on, Query::Visible.new(), Query::Text.new() do |actor, thing, supporter|
+  actor.tell "You don't see anything called \"#{supporter}\" here."
+end
+
+respond :drop_on, Query::Text.new(), Query::Visible.new() do |actor, thing, supporter|
+  actor.tell "You don't see anything called \"#{thing}\" here."
+end
+
+respond :drop_on, Query::Text.new(), Query::Text.new() do |actor, thing, supporter|
+  actor.tell "I don't know what you mean by \"#{thing}\" or \"#{supporter}.\""
 end
 
 xlate "put :thing on :supporter", :drop_on, :thing, :supporter
