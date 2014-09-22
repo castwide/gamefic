d7405af93bed0b2d50a8b74939863816af060fed
Fred Snyder <fsnyder@castwide.com>
2014-09-22 04:22:29 -0400

    Portals handle nonstandard and undefined Directions.

--- lib/gamefic/import/standard/entities/portal.rb
+++ lib/gamefic/import/standard/entities/portal.rb
@@ -1,7 +1,6 @@
 class Portal < Entity
   attr_accessor :destination, :direction
   def find_reverse
-    #rev = Portal.reverse(self.direction)
     rev = direction.reverse
     if rev != nil
       destination.children.that_are(Portal).each { |c|
@@ -15,12 +14,12 @@ class Portal < Entity
   # Portals have distinct direction and name properties so games can display a
   # bare compass direction for exits, e.g., "south" vs. "the southern door."
   def direction
-    @direction || @name
+    @direction ||= Direction.new(:name => @name)
   end
   def name
-    @name || @direction.to_s
+    @name || direction.name
   end
   def synonyms
-    "#{super} #{@direction} #{@direction.adjective} #{@direction.adverb}"
+    "#{super} #{@direction} #{direction.synonyms}"
   end
 end
