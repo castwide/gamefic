9c272293705cea8170f3e21e099c2034f59e036e
Fred Snyder <fsnyder@castwide.com>
2014-09-23 05:56:19 -0400

    HTML spec.

---
+++ spec/html_spec.rb
@@ -0,0 +1,9 @@
+require "gamefic/html"
+include Gamefic
+
+describe Html do
+  it "fixes bare ampersands" do
+    code = "&ldquo;One & two &amp; three, let's go to the A&P&rdquo;"
+    expect(Html.fix_ampersands(code)).to eq("&ldquo;One &amp; two &amp; three, let's go to the A&amp;P&rdquo;")
+  end
+end
