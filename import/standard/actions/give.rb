7d0bb17a16647a39dab295e5c73fc734a0c3bda0
Fred Snyder <fsnyder@castwide.com>
2014-09-24 13:36:22 -0400

    Give action.

---
+++ import/standard/actions/give.rb
@@ -0,0 +1,22 @@
+respond :give, Query::Reachable.new(Character), Query::Reachable.new do |actor, character, gift|
+  if gift.parent != actor
+    actor.perform :take, gift
+  end
+  if gift.parent == actor
+    actor.perform :give, character, gift
+  end
+end
+
+respond :give, Query::Reachable.new, Query::Children.new do |actor, character, gift|
+  actor.tell "Nothing happens."
+end
+
+respond :give, Query::Reachable.new(Character), Query::Children.new do |actor, character, gift|
+  actor.tell "#{The character} doesn't want #{the gift}."
+end
+
+respond :give, Query::Text.new, Query::Children.new do |actor, character, gift|
+  actor.tell "You don't see any \"#{character}\" here."
+end
+
+xlate "give :gift to :character", :give, :character, :gift
