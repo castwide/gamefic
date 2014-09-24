171d4066778d05b0bd0a1075b99492a0d90e04a5
Fred Snyder <fsnyder@castwide.com>
2014-09-24 13:36:07 -0400

    Door entity.

---
+++ import/standard/entities/door.rb
@@ -0,0 +1,36 @@
+import 'standard/entities/portal'
+
+class Door < Portal
+  attr_reader :key
+  def post_initialize
+    super
+    if @name.nil? and !@direction.nil?
+      proper_named = false
+      rev = @direction.reverse
+      self.name = "the #{@direction.adjective} door"
+    end
+  end
+  def key=(entity)
+    @key = entity
+    if !@key.nil?
+      is :openable, :lockable
+    end
+    if !find_reverse.nil?
+      find_reverse.instance_variable_set(:@key, entity)
+    end
+  end
+  def is(*opts)
+    super
+    if !self.find_reverse.nil?
+      opts.each { |opt|
+        find_reverse.option_select opt
+      }
+    end
+  end
+end
+
+options Door, :automatic, :not_automatic
+options Door, :open, :closed, :locked
+options Door, :openable, :not_openable
+options Door, :not_lockable, :lockable
+options Door, :auto_lockable, :not_auto_lockable
