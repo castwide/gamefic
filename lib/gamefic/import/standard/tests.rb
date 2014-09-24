65e88895a4266a2c62ffc09874a8f101576e391b
Fred Snyder <fsnyder@castwide.com>
2014-09-24 06:09:44 -0400

    Standard test logic is completely encapsulated in the Tester module used by Plots.

--- lib/gamefic/import/standard/tests.rb
+++ lib/gamefic/import/standard/tests.rb
@@ -6,27 +6,14 @@ module Tester
     test_procs[name] = block
   end
   def run_test name, actor
-    actor.testing = true
     test_procs[name].call actor
     while actor.queue.length > 0
-      actor.plot.update
+      update
     end
-    actor.testing = false
   end
 end
 class Plot
   include Tester
-end
-
-class Character
-  attr_accessor :testing
-  alias_method :orig_tester_perform, :perform
-  def perform *args
-    if testing == true
-      tell "[TEST] #{state.prompt} #{args.join(' ')}"
-    end
-    orig_tester_perform *args
-  end
 end
 
 meta :test, Query::Text.new do |actor, name|
