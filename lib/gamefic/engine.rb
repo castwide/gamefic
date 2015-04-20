8ea59a72f1015282bdb2ad5ef7fc1381b3a31939
Fred Snyder <fsnyder@castwide.com>
2015-04-19 23:01:48 -0400

    Indentation formatting.

--- lib/gamefic/engine.rb
+++ lib/gamefic/engine.rb
@@ -1,98 +1,98 @@
 module Gamefic
 
-	class Engine
-		def initialize(plot)
-			@plot = plot
+  class Engine
+    def initialize(plot)
+      @plot = plot
       post_initialize
-		end
+    end
     def post_initialize
       @user = User.new @plot
     end
-		def run
+    def run
       @plot.introduce @user.character
       print @user.state.output
-			while @user.character.scene.state != "Concluded"
+      while @user.character.scene.state != "Concluded"
         tick
         print @user.state.output
-			end
-		end
+      end
+    end
     def tick
       @user.stream.select @user.character.scene.prompt
       @user.state.input
       @plot.update
     end
-	end
+  end
 
-	class User
-		attr_reader :state, :character, :story
-		def initialize(plot)
-			@plot = plot
+  class User
+    attr_reader :state, :character, :story
+    def initialize(plot)
+      @plot = plot
       @character = Character.new @plot, :name => 'yourself', :synonyms => 'self myself you me', :proper_named => true
       @character.connect self
       post_initialize
-		end
-    def post_initialize
-			@stream = UserStream.new
-			@state = UserState.new self
     end
-		def stream
-			@stream ||= UserStream.new
-		end
+    def post_initialize
+      @stream = UserStream.new
+      @state = UserState.new self
+    end
+    def stream
+      @stream ||= UserStream.new
+    end
     def state
       @state ||= UserState.new(self)
     end
-		def state=(state_class)
-			@state = state_class.new self
-		end
-		def refresh
-			# Nothing to do
-		end
-		def quit
-			#exit
-		end
-	end
-	
-	class UserStream
-		def initialize
-			@queue = Array.new
+    def state=(state_class)
+      @state = state_class.new self
+    end
+    def refresh
+      # Nothing to do
+    end
+    def quit
+      #exit
+    end
+  end
+  
+  class UserStream
+    def initialize
+      @queue = Array.new
       @buffer = ''
-		end
+    end
     def flush
       tmp = @buffer.clone
       #@buffer.clear
       @buffer = ''
       tmp
     end
-		def send(data)
+    def send(data)
       # Quick and dirty HTML sanitization
       #data.gsub!(/<[a-z]+[^>]*>/i, "")
       #data.gsub!(/<\/[^>]*>/, "")
       @buffer += data
-		end
-		def select(prompt)
-			print prompt + " "
-			line = STDIN.gets
+    end
+    def select(prompt)
+      print prompt + " "
+      line = STDIN.gets
       @queue.push line.strip
-		end
-		def recv
-			@queue.shift
-		end
-	end
-	
-	class UserState
-		attr_reader :user
-		def initialize(user)
-			@user = user
-		end
-		def input
-			line = @user.stream.recv
-			if line != nil
+    end
+    def recv
+      @queue.shift
+    end
+  end
+  
+  class UserState
+    attr_reader :user
+    def initialize(user)
+      @user = user
+    end
+    def input
+      line = @user.stream.recv
+      if line != nil
         @user.character.queue.push line
-			end
-		end
+      end
+    end
     def output
       @user.stream.flush
     end
-	end
+  end
 
 end
