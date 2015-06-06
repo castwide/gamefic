7a01e2f27a953af553c43d052169c2bfd49672fb
Fred Snyder <fsnyder@castwide.com>
2015-06-06 11:57:27 -0400

    Questions import. This import provides basic support for questions asked by players.

---
+++ import/questions.rb
@@ -0,0 +1,14 @@
+respond :what, Query::Text.new do |actor, question|
+  actor.tell "It looks like you're asking a question. Try giving a command instead. Use HELP for more information."
+end
+
+respond :what, Query::Reachable.new do |actor, thing|
+  actor.tell "It looks like you want information about #{the thing}. Try looking at it."
+end
+
+xlate "where :question", "what :question"
+xlate "when :question", "what :question"
+xlate "why :question", "what :question"
+xlate "who :question", "what :question"
+
+xlate "what is :thing", "look :thing"
