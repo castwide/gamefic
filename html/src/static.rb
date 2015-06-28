6d633a67a29106e35ea7a885ca4a0b23b3d5db02
Fred Snyder <fsnyder@castwide.com>
2015-06-28 19:38:05 -0400

    Support for saving and restoring games in cookies.

--- html/src/static.rb
+++ html/src/static.rb
@@ -2,7 +2,15 @@ module Gamefic
   def self.static_plot
     @@static_plot ||= Plot.new
   end
+  class WebUser < User
+    def save
+      `Gamefic.Engine.save();`
+    end
+    def restore
+      `Gamefic.Engine.restore();`
+    end
+  end
   def self.static_player
-    @@static_player ||= User.new(static_plot)
+    @@static_player ||= WebUser.new(static_plot)
   end
 end
