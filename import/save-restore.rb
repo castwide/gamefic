b28bc3576057bc6a1a59a8b5c6b9d0abd1a37ff2
Fred Snyder <fsnyder@castwide.com>
2015-06-28 19:11:12 -0400

    Save and restore are aspects of the engine instead of an import. TTY engine supports user-supplied filenames.

--- import/save-restore.rb
+++ import/save-restore.rb
@@ -2,26 +2,17 @@ require 'json'
 import 'snapshots'
 
 meta :save do |actor|
-  ss = Snapshots.history.last
-  if ss.nil?
-    actor.tell "Nothing to save."
-  else
-    json = JSON.generate(ss)
-    puts json
-    File.open('game.sav', 'w') do |f|
-      f.write json
-    end
-    actor.tell "Game saved."
-  end
+  actor.user.save "game.sav"
+end
+
+meta :save, Query::Text.new() do |actor, filename|
+  actor.user.save filename
 end
 
 meta :restore do |actor|
-  if File.exists?('game.sav')
-    json = File.read('game.sav')
-    data = JSON.parse(json, :symbolize_names => true)
-    Snapshots.restore(data, self)
-    actor.tell "Game restored."
-  else
-    actor.tell "Nothing to restore."
-  end
+  actor.user.restore "game.sav"
+end
+
+meta :restore, Query::Text.new() do |actor, filename|
+  actor.user.restore filename
 end
