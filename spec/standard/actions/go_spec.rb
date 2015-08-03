fed962fe2b5ef36a8167c1992d7d9a22a58dc0a1
Fred Snyder <fsnyder@castwide.com>
2015-08-02 21:57:47 -0400

    Spec for finding portals by destination.

--- spec/standard/actions/go_spec.rb
+++ spec/standard/actions/go_spec.rb
@@ -13,4 +13,14 @@ describe "Go Action" do
     character.perform "go south"
     expect(character.room).to be(room1)
   end
+  it "finds portals by destination" do
+    room1 = @plot.make Room, :name => 'room one'
+    room2 = @plot.make(Room, :name => 'room two')
+    room2.connect room1, "south"
+    character = @plot.make MetaCharacter, :name => 'character', :parent => room1
+    character.perform "go room two"
+    expect(character.room).to be(room2)
+    character.perform "go room one"
+    expect(character.room).to be(room1)  
+  end
 end
