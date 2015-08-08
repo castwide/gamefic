b13c1b05880e63a4a76bf8f73ef63bf07e52556e
Fred Snyder <fsnyder@castwide.com>
2015-08-08 18:27:20 -0400

    Tester module uses a queue for the commands to test.

--- lib/gamefic/tester.rb
+++ lib/gamefic/tester.rb
@@ -5,23 +5,51 @@ module Gamefic
       @test_procs ||= Hash.new
     end
     def on_test name = :me, &block
-      test_procs[name] = block
+      test_procs[name] = TestQueue.new(&block)
     end
     def run_test name, actor
-      test_procs[name].call actor
-      update
-      test_queue = actor.queue.clone
-      actor.queue.clear
-      while test_queue.length > 0
-        actor.tell "#{actor.state.prompt} #{test_queue[0]}"
-        if actor.scene.state == "Paused"
-          actor.queue.unshift ""
+      #test_procs[name].call actor
+      #update
+      #test_queue = actor.queue.clone
+      #actor.queue.clear
+      #while test_queue.length > 0
+      #  actor.tell "#{actor.scene.data.prompt} #{test_queue[0]}"
+      #  if actor.scene.state == "Paused"
+      #    actor.queue.unshift ""
+      #  else
+      #    actor.queue.unshift test_queue.shift
+      #  end
+      #  update
+      #end
+      queue = test_procs[name].queue
+      actor.tell "Gonna try! #{queue.join("\n")}"
+      while queue.length > 0
+        act = queue.shift
+        if act.kind_of?(String)
+          actor.queue.push act
         else
-          actor.queue.unshift test_queue.shift
+          act.call actor
         end
         update
+        while (actor.scene.state == 'Paused')
+          actor.queue.push ''
+          update
+        end
       end
     end
+    
+    class TestQueue
+      include Stage
+      expose :queue
+      def queue
+        @queue ||= []
+      end
+      def initialize &block
+        stage &block
+        puts queue.join("\n")
+      end
+    end
+    
   end
 
 end
