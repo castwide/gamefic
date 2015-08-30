bdb39bec4cc9ad4438b1b4987def1656dcf8cc8d
Fred Snyder <fsnyder@castwide.com>
2015-08-30 13:51:13 -0400

    Tester outputs prompts, commands, and total number of commands tested.

--- lib/gamefic/tester.rb
+++ lib/gamefic/tester.rb
@@ -10,19 +10,23 @@ module Gamefic
     def run_test name, actor
       queue = []
       actor.plot.stage actor, queue, &test_procs[name]
+      queue_count = queue.length
       while queue.length > 0
         act = queue.shift
         if act.kind_of?(String)
+          actor.stream actor.scene.data.prompt + " " + act + "\n\n"
           actor.queue.push act
         else
           act.call actor
         end
         update
         while (actor.scene.state == 'Paused')
+          actor.stream actor.scene.data.prompt
           actor.queue.push ''
           update
         end
       end
+      actor.tell "(#{queue_count} commands tested.)"
     end    
   end
 
