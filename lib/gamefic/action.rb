d1c658dd19c752f9b6e82e0ee3a6c78ad4625c66
Fred Snyder <fsnyder@castwide.com>
2015-09-20 03:02:24 -0400

    Formatting.

--- lib/gamefic/action.rb
+++ lib/gamefic/action.rb
@@ -1,9 +1,9 @@
 module Gamefic
 
-	class Action
-		attr_reader :order_key, :caller
+  class Action
+    attr_reader :order_key, :caller
     @@order_key_seed = 0
-		def initialize(story, command, *queries, &proc)
+    def initialize(story, command, *queries, &proc)
       if !command.kind_of?(Symbol)
         command = command.to_s
         command = nil if command == ''
@@ -11,49 +11,49 @@ module Gamefic
       @plot = story
       @order_key = @@order_key_seed
       @@order_key_seed += 1
-			@proc = proc
-			if (command.kind_of?(Symbol) == false and !command.nil?)
-				raise "Action commands must be symbols"
-			end
+      @proc = proc
+      if (command.kind_of?(Symbol) == false and !command.nil?)
+        raise "Action commands must be symbols"
+      end
       if !@proc.nil?
         if (queries.length + 1 != @proc.arity) and (queries.length == 0 and @proc.arity != -1)
           raise "Number of queries is not compatible with proc arguments"
         end
       end
-			@command = command
-			@queries = queries
+      @command = command
+      @queries = queries
       if !story.nil?
         story.send :add_action, self
       end
-		end
+    end
     def block
       @proc
     end
-		def specificity
-			spec = 0
-			magnitude = 1
-			@queries.each { |q|
-				if q.kind_of?(Query::Base)
-					spec += (q.specificity * magnitude)
-				else
-					spec += magnitude
-				end
-				#magnitude = magnitude * 10
-			}
-			return spec
-		end
+    def specificity
+      spec = 0
+      magnitude = 1
+      @queries.each { |q|
+        if q.kind_of?(Query::Base)
+          spec += (q.specificity * magnitude)
+        else
+          spec += magnitude
+        end
+        #magnitude = magnitude * 10
+      }
+      return spec
+    end
     def verb
       @command
     end
     def command
       @command
     end
-		def key
-			@key
-		end
-		def queries
-			@queries
-		end
+    def key
+      @key
+    end
+    def queries
+      @queries
+    end
     def execute *args
       @proc.call(*args)
     end
@@ -64,18 +64,18 @@ module Gamefic
       }
       sig.join(', ').gsub(/Gamefic::/, '')
     end
-		private
-			def self.explode(entity)
-				arr = Array.new
-				arr.push entity
-				cls = entity.class
-				while cls != Object
-					arr.push cls
-					cls = cls.superclass
-				end
-				arr.push String
-				arr.push nil
-			end
-	end
+    private
+      def self.explode(entity)
+        arr = Array.new
+        arr.push entity
+        cls = entity.class
+        while cls != Object
+          arr.push cls
+          cls = cls.superclass
+        end
+        arr.push String
+        arr.push nil
+      end
+  end
 
 end
