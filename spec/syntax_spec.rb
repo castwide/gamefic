398fcb7554e828225f140c38ba660b5a1718bd3d
Fred Snyder <fsnyder@castwide.com>
2015-09-24 23:18:39 -0400

    Syntax bugs.

--- spec/syntax_spec.rb
+++ spec/syntax_spec.rb
@@ -22,5 +22,25 @@ describe Syntax do
     end
     expect(Syntax.match("one", plot.syntaxes).length).to eq(1)
     expect(Syntax.match("one two", plot.syntaxes).length).to eq(1)
-  end 
+  end
+  it "passes the remainder of an argument to the next argument" do
+    plot = Plot.new
+    item = nil
+    plot.respond :combine, Query.siblings do |actor, i1|
+      item = i1
+    end
+    plot.respond :combine, Query.siblings, Query.siblings do |actor, i1, i2|
+      item = [i1, i2]
+    end
+    room = plot.make Room, :name => "room"
+    foo = plot.make Entity, :name => "foo", :parent => room
+    bar = plot.make Entity, :name => "bar", :parent => room
+    character = plot.make Character, :name => "character", :parent => room
+    character.perform "combine foo"
+    expect(item).to eq(foo)
+    character.perform "combine bar"
+    expect(item).to eq(bar)
+    character.perform "combine foo bar"
+    expect(item).to eq([foo, bar])
+  end
 end
