5c0b5dd7b9c40c6a3ffd83917ed56462131d8a7d
Fred Snyder <fsnyder@castwide.com>
2015-10-04 21:06:37 -0400

    Comment to explain need for String#capitalize_first.

--- lib/gamefic/core_ext/string.rb
+++ lib/gamefic/core_ext/string.rb
@@ -1,4 +1,6 @@
 class String
+  # Capitalize the first letter without changing the rest of the string.
+  # (String#capitalize makes the rest of the string lower-case.)
 	def capitalize_first
 		"#{self[0,1].upcase}#{self[1,self.length]}"
 	end
