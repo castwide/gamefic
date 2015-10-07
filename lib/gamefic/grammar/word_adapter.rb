0d4718171274c14953ff4a81b309756ac6e5062f
Fred Snyder <fsnyder@castwide.com>
2015-10-06 20:05:03 -0400

    Experimental contract method.

--- lib/gamefic/grammar/word_adapter.rb
+++ lib/gamefic/grammar/word_adapter.rb
@@ -2,13 +2,13 @@ require 'gamefic/grammar'
 
 module Gamefic::Grammar
   module WordAdapter
-    def pronoun #=> Grammar::Pronouns
+    def pronoun
       @pronoun ||= Grammar::Pronouns.new(self)
     end
     def verb
       @verb ||= Grammar::Verbs.new(self)
     end
-    def contract infinitive
+    def contract words
       @contractions ||= {
         "I am" => "I'm",
         "you are" => "you're",
@@ -16,9 +16,14 @@ module Gamefic::Grammar
         "she is" => "she's",
         "it is" => "it's",
         "we are" => "we're",
-        "they are" => "they're"
+        "they are" => "they're",
+        "am not" => "am not",
+        "are not" => "aren't",
+        "is not" => "isn't",
+        "do not" => "don't",
+        "does not" => "doesn't"
       }
-      src = pronoun.subj + " " + verb.send(infinitive)
+      src = pronoun.subj + " " + verb.send(words)
       @contractions[src] || src
     end
   end
