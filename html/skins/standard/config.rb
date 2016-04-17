61fb3d004637886c40b2d041c7080d3ffe203f23
Fred Snyder <fsnyder@castwide.com>
2016-04-17 01:31:17 -0400

    First version of standard skin.

---
+++ html/skins/standard/config.rb
@@ -0,0 +1,9 @@
+javascripts.push "core/opal.js"
+javascripts.push "core/gamefic.js"
+javascripts.push "core/static.js"
+javascripts.push "core/scripts.js"
+javascripts.push "core/engine.js"
+javascripts.push "core/jquery.js"
+javascripts.push "play.js"
+
+stylesheets.push "style.css"
---
+++ html/skins/standard/play.js
@@ -0,0 +1,61 @@
+$(function() {
+	$('#gamefic_controls form').submit(function(event) {
+		event.preventDefault();
+		Gamefic.update($('#gamefic_command').val());
+	});
+	Gamefic.onStart(function(response) {
+		var prompt = response.prompt;
+		if (prompt == '>') {
+			prompt = 'What do you want to do?'
+		}
+		$('#gamefic_prompt').html(prompt);
+		$('#gamefic_output a[rel="gamefic"]').addClass('disabled');
+	});
+	Gamefic.onInput(function(response) {
+		if (response.input != null) {
+			$('#gamefic_output').append('<p><kbd>' + response.prompt + ' ' + response.input + '</kbd></p>');
+		}
+	});
+	Gamefic.onFinish(function(response) {
+		$('#gamefic_command').val('');
+		$('#gamefic_command').focus();
+		window.scrollTo(0, document.body.scrollHeight);
+	});
+	Gamefic.handleResponse('Active', function(response) {
+		$('#gamefic_output').append(response.output);
+	});
+	Gamefic.handleResponse('YesOrNo', function(response) {
+		$('#gamefic_output').append(response.output);
+		$('#gamefic_output').append('<p>(<a href="#" rel="gamefic" data-command="yes">Yes</a> or <a href="#" rel="gamefic" data-command="no">No</a>)</p>');
+	});
+	Gamefic.handleResponse('MultipleChoice', function(response) {
+		var jq = $('<div/>');
+		jq.html(response.output);
+		jq.find('ol.multiple_choice li').each(function() {
+			var item = $(this).text();
+			var link = $('<a/>');
+			link.attr('href', '#');
+			link.attr('rel', 'gamefic');
+			link.attr('data-command', item);
+			link.text(item);
+			$(this).html(link);
+		});
+		$('#gamefic_output').append(jq);
+	});
+	Gamefic.handleResponse('Concluded', function(response) {
+		if (response.input != null) {
+			$('#gamefic_output').append('<p><kbd>' + response.prompt + ' ' + response.input + '</kbd></p>');
+		}
+		$('#gamefic_console').addClass('concluded');
+		$('#gamefic_output').append(response.output);
+		$('#gamefic_controls').hide();
+	});
+	$('#gamefic_output').on('click', 'a[rel="gamefic"]', function(event) {
+		event.preventDefault();
+		if (!$(this).hasClass('disabled')) {
+			Gamefic.update($(this).attr('data-command'));
+		}
+	});
+	Gamefic.start();
+	$('#gamefic_command').focus();
+});
