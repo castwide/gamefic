69c48492d88b708c1361d8a4d823aa3036a4cb8c
Fred Snyder <fsnyder@castwide.com>
2016-04-18 07:49:42 -0400

    Subcommand base interface.

--- lib/gamefic/shell/subcommand/base.rb
+++ lib/gamefic/shell/subcommand/base.rb
@@ -1,8 +1,16 @@
 class Gamefic::Shell::Subcommand::Base
-  def execute *args
-    
+  # Execute the command
+  #
+  def run
+    raise "Unimplemented command"
   end
+  
+  # Get the documentation for this command.
+  # The gamefic executable will provide this text in response to
+  # "gamefic help [command]"
+  #
+  # @return [String]
   def help
-    
+    "Help is not available for this command."
   end
 end
