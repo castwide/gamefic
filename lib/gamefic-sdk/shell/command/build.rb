597aa1af7240e9c94ce3f2867ee55c96ad1d00b3
Fred Snyder <fsnyder@castwide.com>
2016-04-20 10:31:43 -0400

    Fixed gfk build bug and added specs.

--- lib/gamefic-sdk/shell/command/build.rb
+++ lib/gamefic-sdk/shell/command/build.rb
@@ -1,9 +1,12 @@
 require 'gamefic-sdk/build'
 
 class Gamefic::Sdk::Shell::Command::Build < Gamefic::Shell::Command::Base
-  def run
-    result parse input
+  def initialize
+    options.boolean '-q', '--quiet', 'suppress output', default: false
+  end
+  
+  def run input
     result = parse input
-    Gamefic::Sdk::Build.release result.arguments[1]
+    Gamefic::Sdk::Build.release result.arguments[1], result['--quiet']
   end  
 end
--- spec/gamefic-sdk/shell_spec.rb
+++ spec/gamefic-sdk/shell_spec.rb
@@ -4,16 +4,20 @@ require 'gamefic-sdk/shell'
 include Gamefic
 include Gamefic::Sdk
 
-describe Gamefic::Sdk::Shell::Command::Init do
+describe Gamefic::Sdk::Shell::Command do
   before :all do
     @dir = Dir.mktmpdir
   end
   it "initializes a game source directory" do
-    ARGV.clear
-    ARGV.push 
     command = Gamefic::Sdk::Shell::Command::Init.new
     command.run ['init', "#{@dir}/game", "-q"]
     expect(File.exist?("#{@dir}/game")).to eq(true)
+  end
+  it "builds a game" do
+    command = Gamefic::Sdk::Shell::Command::Build.new
+    command.run ['build', "#{@dir}/game", "-q"]
+    expect(File.exist?("#{@dir}/game/release/gfic/game.gfic")).to eq(true)
+    expect(File.exist?("#{@dir}/game/release/web")).to eq(true)
   end
   after :all do
     FileUtils.remove_entry_secure @dir
