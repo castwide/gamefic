8c9f9617e485b358deb037918ec945d0479d9635
Fred Snyder <fsnyder@castwide.com>
2016-08-11 07:44:29 -0400

    Documentation.

--- lib/gamefic/script/base.rb
+++ lib/gamefic/script/base.rb
@@ -11,18 +11,23 @@ module Gamefic
     def read
       raise "#read must be defined in subclasses"
     end
-    # Get the script's path
+    # Get the script's path.
     #
     # @return [String]
     def path
       raise "#path must be defined in subclasses"
     end
-    # Get the absolute path of the script's original file
+    # Get the absolute path of the script's original file, or its URL for
+    # sources that are not file-based.
     #
     # @return [String]
     def absolute_path
       raise "#absolute_path must be defined in subclasses"
     end
+    # Script objects are equal if their relative paths are the same. Note that
+    # they are still considered equal if their absolute paths are different,
+    # or even if they come from different types of sources.
+    #
     # @param other[Script::Base]
     # @return [Boolean]
     def==(other)
