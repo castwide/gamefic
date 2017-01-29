b449c35f4af8e968da7a1e2d0c580de944a4296e
Fred Snyder <fsnyder@castwide.com>
2017-01-29 09:07:05 -0500

    Buffer documentation.

--- lib/gamefic/user/buffer.rb
+++ lib/gamefic/user/buffer.rb
@@ -1,14 +1,20 @@
 module Gamefic
 
+  # A simple buffer class for collecting and returning data received from
+  # plots.
   class User::Buffer
     def initialize
       @data = ''
     end
 
+    # Append a message to the buffer.
     def send message
       @data += message
     end
 
+    # Get the current data and clear the buffer.
+    #
+    # @return [String]
     def flush
       tmp = @data
       @data = ''
