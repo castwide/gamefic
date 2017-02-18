dbceb205df773361b1f7e52dab2bc4ada98dd1b9
Fred Snyder <fsnyder@castwide.com>
2017-02-18 15:29:39 -0500

    Subplot spec checks for destruction of entities on itself instead of parent.

--- spec/gamefic/subplot_spec.rb
+++ spec/gamefic/subplot_spec.rb
@@ -3,7 +3,8 @@ describe Subplot do
     plot = Plot.new
     subplot = Subplot.new(plot)
     entity = subplot.make(Entity, name: 'entity')
+    expect(subplot.entities.include? entity).to be(true)
     subplot.conclude
-    expect(plot.entities.include? entity).to be(false)
+    expect(subplot.entities.include? entity).to be(false)
   end
 end
