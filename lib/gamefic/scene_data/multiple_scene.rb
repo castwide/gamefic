c321393cf9f50a72776402526be44ba88c421f17
Fred Snyder <fsnyder@castwide.com>
2017-04-14 18:48:58 -0400

    Refactored MultipleScene option handling.

--- lib/gamefic/scene_data/multiple_scene.rb
+++ lib/gamefic/scene_data/multiple_scene.rb
@@ -1,40 +1,24 @@
 module Gamefic
 
   class SceneData::MultipleScene < SceneData::MultipleChoice
-    def options
-      #scene_map.keys
-      p_option_keys.clone
+    def clear
+      options.clear
+      scene_map.clear
     end
 
-    def clear
-      p_option_keys.clear
-      p_option_scenes.clear
-    end
-    
     def map choice, scene
-      #scene_map[choice] = scene
-      p_option_keys.push choice
-      p_option_scenes.push scene
+      options.push choice
+      scene_map[choice] = scene
     end
 
     def scene_for choice
-      #scene_map[choice]
-      index = p_option_keys.index(choice)
-      p_option_scenes[index]
-    end
-
-    def scene_map
-      @scene_map ||= {}
+      scene_map[choice]
     end
 
     private
 
-    def p_option_keys
-      @p_option_keys ||= []
-    end
-
-    def p_option_scenes
-      @p_option_scenes ||= []
+    def scene_map
+      @scene_map ||= {}
     end
   end
 
