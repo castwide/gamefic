d0a72fbb21a380db69c7500b86fdce4540b2c4e4
Fred Snyder <fsnyder@castwide.com>
2017-04-19 01:24:32 -0400

    Documented Array and deprecated redundant shuffle methods.

--- lib/gamefic/core_ext/array.rb
+++ lib/gamefic/core_ext/array.rb
@@ -1,4 +1,15 @@
 class Array
+  # Get a subset of the array that matches the argument.
+  # If the argument is a Class or Module, the elements must be of the type.
+  # If the argument is a Symbol, it should be a method for which the elements must return true.
+  # If the argument is an Object, the elements must equal the object.
+  #
+  # @example
+  #   animals = ['dog', 'cat', nil]
+  #   animals.that_are(String) #=> ['dog', 'cat']
+  #   animals.that_are('dog')  #=> ['dog']
+  #   animals.that_are(:nil?)  #=> [nil]
+  #
   # @return [Array]
   def that_are(cls)
     if (cls.kind_of?(Class) or cls.kind_of?(Module))
@@ -12,6 +23,10 @@ class Array
       return Array.new
     end
   end
+
+  # Get a subset of the array that does not match the argument.
+  # See Array#that_are for information about how arguments are evaluated.
+  #
   # @return [Array]
   def that_are_not(cls)
     if (cls.kind_of?(Class) or cls.kind_of?(Module))
@@ -22,26 +37,50 @@ class Array
       return self.clone - [cls]
     end
   end
+
+  # Get a random element from the array.
+  # @deprecated Use Array#sample instead.
+  #
   def random
     return self[rand(self.length)]
   end
+
+  # Pop a random element from the array.
+  # @deprecated Use Array#pop_sample instead.
+  #
   def pop_random
+    pop_sample
+  end
+
+  # Pop a random element from the array.
+  #
+  def pop_sample
     delete_at(rand(self.length))
   end
+
+  # @todo Candidate for deprecation
   # @return [Array]
-  def shuffle
-    self.sort { |a, b|
-      rand(3) <=> rand(3)
-    }
-  end
+  #def shuffle
+  #  self.sort { |a, b|
+  #    rand(3) <=> rand(3)
+  #  }
+  #end
+
+  # @todo Candidate for deprecation
   # @return [Array]
-  def shuffle!
-    self.sort! { |a, b|
-      rand(3) <=> rand(3)
-    }
-  end
-  # Get a string representation of the array that separated elements with
+  #def shuffle!
+  #  self.sort! { |a, b|
+  #    rand(3) <=> rand(3)
+  #  }
+  #end
+
+  # Get a string representation of the array that separates elements with
   # commas and adds a conjunction before the last element.
+  #
+  # @example
+  #   animals = ['a dog', 'a cat', 'a mouse']
+  #   animals.join_and #=> 'a dog, a cat, and a mouse'
+  #
   # @return [String]
   def join_and(sep = ', ', andSep = ' and ', serial = true)
     if self.length < 3
@@ -51,6 +90,9 @@ class Array
       start.join(sep) + "#{serial ? sep.strip : ''}#{andSep}#{self.last}"
     end
   end
+
+  # @see Array#join_and
+  #
   # @return [String]
   def join_or(sep = ', ', orSep = ' or ', serial = true)
     join_and(sep, orSep, serial)
