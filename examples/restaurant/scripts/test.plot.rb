09cb50e4ae33bf3c1c30a52fa386bb9143bb6efa
Fred Snyder <fsnyder@castwide.com>
2017-05-06 04:10:26 -0400

    Renamed example plot scripts.

---
+++ examples/restaurant/scripts/test.plot.rb
@@ -0,0 +1,14 @@
+script 'standard/test'
+
+on_test :me do |actor, queue|
+  queue.push "n"
+  queue.push "nw"
+  queue.push "eat dinner"
+  queue.push "se"
+  queue.push "s"
+  queue.push "w"
+  queue.push "take jacket"
+  queue.push "wear jacket"
+  queue.push "e"
+  queue.push "s"
+end
