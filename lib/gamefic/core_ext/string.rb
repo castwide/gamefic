d8ec90cdef0e322d5c6777b79e0fd51c79418e4a
Fred Snyder <fsnyder@castwide.com>
2017-05-31 10:03:28 -0400

    Formatting.

--- lib/gamefic/core_ext/string.rb
+++ lib/gamefic/core_ext/string.rb
@@ -1,16 +1,16 @@
 class String
-	include Gamefic::Matchable
+  include Gamefic::Matchable
   # Capitalize the first letter without changing the rest of the string.
   # (String#capitalize makes the rest of the string lower-case.)
-	def capitalize_first
-		"#{self[0,1].upcase}#{self[1,self.length]}"
-	end
-	# @return [String]
-	def cap_first
-		self.capitalize_first
-	end
-	# @return [Array]
-	def split_words
-		self.gsub(/ +/, ' ').strip.split
-	end
+  def capitalize_first
+    "#{self[0,1].upcase}#{self[1,self.length]}"
+  end
+  # @return [String]
+  def cap_first
+    self.capitalize_first
+  end
+  # @return [Array]
+  def split_words
+    self.gsub(/ +/, ' ').strip.split
+  end
 end
