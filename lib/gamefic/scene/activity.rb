4fa34a52aee1e17c8bf5e00de73d9ffdc6a97149
Fred Snyder <fsnyder@castwide.com>
2017-06-03 22:44:55 -0400

    Catch empty input in Activity.

--- lib/gamefic/scene/activity.rb
+++ lib/gamefic/scene/activity.rb
@@ -12,7 +12,7 @@ module Gamefic
     def finish
       super
       o = nil
-      o = actor.perform input.strip unless input.nil?
+      o = actor.perform input.strip unless input.to_s.strip.empty?
       actor.performed o
     end
 
