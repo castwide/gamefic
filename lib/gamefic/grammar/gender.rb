13b853fbbbb1daecf95f51d60d88a1365f214947
Fred Snyder <fsnyder@castwide.com>
2017-11-22 20:17:14 -0500

    Gender symbols.

--- lib/gamefic/grammar/gender.rb
+++ lib/gamefic/grammar/gender.rb
@@ -2,18 +2,18 @@ require 'gamefic/grammar'
 
 module Gamefic::Grammar
   module Gender
-    MALE =    'male'.freeze
-    FEMALE =  'female'.freeze
-    OTHER =   'other'.freeze
-    NEUTRAL = 'neutral'.freeze
+    MALE    = :male
+    FEMALE  = :female
+    OTHER   = :other
+    NEUTRAL = :neutral
 
     attr_writer :gender
 
-    # Supported values are "male", "female", "other", and "neutral"
+    # Supported values are :male, :female, :other, and :neutral.
     #
     # @return [String]
     def gender
-      @gender ||= "neutral"
+      @gender ||= :neutral
     end
   end
 end
