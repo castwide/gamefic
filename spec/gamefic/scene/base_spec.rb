627aeb45198d2e6de4a745087a15137bca5c1ad7
Fred Snyder <fsnyder@castwide.com>
2017-11-26 23:02:31 -0500

    Scene specs.

--- spec/gamefic/scene/base_spec.rb
+++ spec/gamefic/scene/base_spec.rb
@@ -6,4 +6,15 @@ describe Gamefic::Scene::Base do
     base.finish
     expect(base.finished?).to be(true)
   end
+
+  it "tracks entries" do
+    klass = Gamefic::Scene::Base.subclass
+    klass.tracked = true
+    actor = Gamefic::Actor.new
+    expect(actor.entered?(klass)).to be(false)
+    scene = klass.new(actor)
+    scene.start
+    expect(actor.entered?(klass)).to be(true)
+    expect(actor.entered?(scene)).to be(true)
+  end
 end
--- spec/gamefic/scene/multiple_scene_spec.rb
+++ spec/gamefic/scene/multiple_scene_spec.rb
@@ -13,4 +13,22 @@ describe Scene::MultipleScene do
     plot.update
     expect(character.will_cue? scene1).to be true
   end
+
+  it "tracks entered selections" do
+    actor = Gamefic::Actor.new
+    klass1 = Scene::Base.subclass
+    klass1.tracked = true
+    klass2 = Scene::Base.subclass
+    klass2.tracked = true
+    selector = Gamefic::Scene::MultipleScene.subclass do |actor, scene|
+      scene.map 'one', klass1
+      scene.map 'two', klass2
+    end
+    scene1 = klass1.new(actor)
+    scene1.start
+    selobj = selector.new(actor)
+    selobj.start
+    expect(selobj.state[:entered]['one']).to be(true)
+    expect(selobj.state[:entered]['two']).to be(false)
+  end
 end
