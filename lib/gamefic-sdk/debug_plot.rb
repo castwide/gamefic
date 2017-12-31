848ba902e37590e15900733c944657d7fe490412
Fred Snyder <fsnyder@castwide.com>
2017-12-31 16:42:03 -0500

    Include meta actions in debug info. Fixed action sorting.

--- lib/gamefic-sdk/debug_plot.rb
+++ lib/gamefic-sdk/debug_plot.rb
@@ -3,7 +3,13 @@ module Gamefic
     class DebugPlot < Gamefic::Plot
       def respond *args, &block
         result = super
-        action_meta.push({verb: result.verb, signature: result.signature, location: get_location(caller), filename: get_location(caller).split(':')[0..-2].join(':'), line: get_location(caller).split(':').last, object: result})
+        log_action result
+        result
+      end
+
+      def meta *args, &block
+        result = super
+        log_action result
         result
       end
 
@@ -18,13 +24,25 @@ module Gamefic
       end
 
       def action_info
-        action_meta.sort_by.with_index{|a, i| [a[:object].rank, -i]}.reverse
+        action_meta.sort_by.with_index{|a, i| [a[:object].rank, i]}.reverse
       end
 
       private
 
       def action_meta
         @action_meta ||= []
+      end
+
+      def log_action result
+        action_meta.push({
+          verb: result.verb,
+          signature: result.signature,
+          location: get_location(caller),
+          filename: get_location(caller).split(':')[0..-2].join(':'),
+          line: get_location(caller).split(':').last,
+          type: (result.meta? ? 'meta' : 'action'),
+          object: result
+        })
       end
 
       def get_location calls
