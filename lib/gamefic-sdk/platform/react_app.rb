dd93769622d85c51665e55d8573a2d95439b9ea1
Fred Snyder <fsnyder@castwide.com>
2018-10-13 18:32:06 -0400

    ReactApp updates.

--- lib/gamefic-sdk/platform/react_app.rb
+++ lib/gamefic-sdk/platform/react_app.rb
@@ -8,7 +8,7 @@ module Gamefic
           FileUtils.mkdir_p target_dir
           write_files_to_target File.join(Gamefic::Sdk::PLATFORMS_PATH, 'reactapp')
           Dir.chdir target_dir do
-            system "npm", "install", "webpack", "webpack-dev-server", "babel-core", "babel-loader", "babel-preset-env", "babel-preset-react", "css-loader", "style-loader", "script-loader", "file-loader", "image-webpack-loader", "webpack-synchronizable-shell-plugin", "copy-webpack-plugin", "--save-dev"
+            system "npm", "install", "webpack", "webpack-dev-server", "babel-loader@7", "babel-core", "babel-preset-env", "babel-preset-react", "css-loader", "style-loader", "script-loader", "file-loader", "image-webpack-loader", "webpack-synchronizable-shell-plugin", "copy-webpack-plugin", "--save-dev"
             system "npm", "install", "react", "react-dom", "--save"
             system "npm", "install", "gamefic-driver", "react-gamefic", "--save"
           end
@@ -22,6 +22,7 @@ module Gamefic
             Gamefic::Sdk::Server.run!
           end
           Dir.chdir target_dir do
+            STDERR.puts "Target dir: #{target_dir}"
             pid = Process.spawn "npm", "run", "start"
             Process.wait pid
           end
