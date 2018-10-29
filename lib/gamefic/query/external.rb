a659795a3464ab9b9d2fba28184c4b2f3a52b405
Fred Snyder <fsnyder@castwide.com>
2018-10-28 21:42:30 -0400

    Query::External#accept? filters by included entities.

--- lib/gamefic/query/external.rb
+++ lib/gamefic/query/external.rb
@@ -9,6 +9,10 @@ module Gamefic
       def context_from subject
         @objects
       end
+
+      def accept?(entity)
+        @objects.include?(entity) && super(entity)
+      end
     end
   end
 end
