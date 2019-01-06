c60ea0a8484abd7d04ef175bebcf090f300d6dd5
Fred Snyder <fsnyder@castwide.com>
2019-01-06 00:37:35 -0500

    Standard Entity.

--- scripts/standard/entities.plot.rb
+++ scripts/standard/entities.plot.rb
@@ -1,7 +1,5 @@
 script 'standard/entities/thing'
 script 'standard/entities/character'
-#script 'standard/entities/container'
-#script 'standard/entities/door'
 script 'standard/entities/fixture'
 script 'standard/entities/item'
 script 'standard/entities/portal'
@@ -10,3 +8,5 @@ script 'standard/entities/room'
 script 'standard/entities/scenery'
 script 'standard/entities/rubble'
 script 'standard/entities/supporter'
+
+class Entity < Gamefic::Entity; end
