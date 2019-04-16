a2d9cdb605def010d2a7673e144979fd499fa333
Fred Snyder <fsnyder@castwide.com>
2019-04-16 15:43:38 -0400

    Stale Players methods.

--- lib/gamefic/world/players.rb
+++ lib/gamefic/world/players.rb
@@ -10,23 +10,10 @@ module Gamefic
         entities.that_are(Gamefic::Active).reject{|e| e.user.nil?}
       end
 
-      # Set the character that the player will control on introduction.
-      #
-      # @param actor [Gamefic::Actor]
-      def set_player_character actor
-        @player_character = actor
-      end
-
       # Get the character that the player will control on introduction.
       #
       # @return [Gamefic::Actor]
       def get_player_character
-        @player_character || make_player_character
-      end
-
-      private
-
-      def make_player_character
         cast player_class, name: 'yourself', synonyms: 'self myself you me', proper_named: true
       end
     end
