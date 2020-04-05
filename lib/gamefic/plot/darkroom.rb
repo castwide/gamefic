61af8f77048595dd4be815a1588a939bfbebf273
Fred Snyder <fsnyder@castwide.com>
2020-04-05 09:01:36 -0400

    Darkroom uses namespace_to_constant.

--- lib/gamefic/plot/darkroom.rb
+++ lib/gamefic/plot/darkroom.rb
@@ -35,7 +35,7 @@ module Gamefic
           if elematch
             klass = index[elematch[1].to_i]
           else
-            klass = eval(obj['class'])
+            klass = namespace_to_constant(obj['class'])
           end
           index.push klass.allocate
         end
@@ -69,40 +69,6 @@ module Gamefic
           space = space.const_get(part)
         end
         space
-      end
-
-      def serialize_subplot s
-        {
-          'class' => s.class.to_s,
-          'entities' => s.entities.map(&:to_serial),
-          'instance_variables' => s.serialize_instance_variables,
-          'theater_instance_variables' => s.theater.serialize_instance_variables
-        }
-      end
-
-      def unserialize_subplot s
-        cls = namespace_to_constant(s['class'])
-        sp = cls.allocate
-        sp.instance_variable_set(:@plot, plot)
-        s['entities'].each do |e|
-          sp.entities.push Gamefic::Index.from_serial(e)
-        end
-        s['instance_variables'].each_pair do |k, v|
-          next if v == "#<UNKNOWN>"
-          sp.instance_variable_set(k, Gamefic::Index.from_serial(v))
-        end
-        s['theater_instance_variables'].each_pair do |k, v|
-          next if v == "#<UNKNOWN>"
-          sp.theater.instance_variable_set(k, Gamefic::Index.from_serial(v))
-        end
-        plot.subplots.push sp
-        sp.send(:run_scripts)
-        # @todo Assuming one player
-        if plot.players.first
-          sp.players.push plot.players.first
-          plot.players.first.playbooks.push sp.playbook unless plot.players.first.playbooks.include?(sp.playbook)
-        end
-        sp
       end
     end
   end
