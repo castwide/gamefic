0cfd0ec75843921ab4a6d2f15b2c0dccdb0e3ed7
Fred Snyder <fsnyder@castwide.com>
2020-08-08 18:04:06 -0400

    Scene method specs.

--- spec/world/scenes_spec.rb
+++ spec/world/scenes_spec.rb
@@ -13,4 +13,44 @@ describe Gamefic::World::Scenes do
     ps = object.pause { puts 'test' }
     expect(object.scene_classes).to include(ps)
   end
+
+  it 'creates a conclusion' do
+    actor = Gamefic::Actor.new
+    conclusion = object.conclusion do |actor|
+      actor.tell "Concluded"
+    end
+    scene = conclusion.new(actor)
+    expect(scene).to be_a(Gamefic::Scene::Conclusion)
+    run scene
+    expect(actor.messages).to include("Concluded")
+  end
+
+  it 'creates a question' do
+    actor = Gamefic::Actor.new
+    question = object.question do |actor, data|
+      actor.tell data.input
+    end
+    scene = question.new(actor)
+    expect(scene).to be_a(Gamefic::Scene::Custom)
+    actor.queue.push "the answer"
+    run scene
+    expect(actor.messages).to include("the answer")
+  end
+
+  it 'creates a custom scene' do
+    actor = Gamefic::Actor.new
+    custom = object.custom do |actor|
+      actor.tell "Custom"
+    end
+    scene = custom.new(actor)
+    expect(scene).to be_a(Gamefic::Scene::Custom)
+    run scene
+    expect(actor.messages).to include("Custom")
+  end
+
+  def run scene
+    scene.start
+    scene.update
+    scene.finish
+  end
 end
