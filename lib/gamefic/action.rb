d284045a826b24910ba30cc126b0fb9a03693932
Fred Snyder <fsnyder@castwide.com>
2020-10-07 13:12:06 -0400

    Action refactoring and documentation.

--- lib/gamefic/action.rb
+++ lib/gamefic/action.rb
@@ -54,25 +54,32 @@ module Gamefic
       self.class.meta?
     end
 
-    def self.subclass verb, *q, meta: false, &block
+    # @param verb [Symbol]
+    # @param queries [Array<Gamefic::Query::Base]
+    # @param meta [Boolean]
+    # @return [Class<Action>]
+    def self.subclass verb, *queries, meta: false, &block
       act = Class.new(self) do
         self.verb = verb
         self.meta = meta
-        q.each { |q|
+        queries.each do |q|
           add_query q
-        }
+        end
         on_execute &block
       end
-      if !block.nil? and act.queries.length + 1 != block.arity and block.arity > 0
+      if !block.nil? && act.queries.length + 1 != block.arity && block.arity > 0
         raise ActionArgumentError.new("Number of parameters is not compatible with proc arguments")
       end
       act
     end
 
     class << self
-      def verb
-        @verb
-      end
+      attr_reader :verb
+
+      # The proc to call when the action is executed
+      #
+      # @return [Proc]
+      attr_reader :executor
 
       def meta?
         @meta ||= false
@@ -93,7 +100,7 @@ module Gamefic
 
       def signature
         # @todo This is clearly unfinished
-        "#{verb} #{queries.map{|m| m.signature}.join(', ')}"
+        "#{verb} #{queries.map {|m| m.signature}.join(', ')}"
       end
 
       # True if this action is not intended to be performed directly by a
@@ -107,19 +114,13 @@ module Gamefic
         verb.to_s.start_with?('_')
       end
 
-      # The proc to call when the action is executed
-      #
-      # @return [Proc]
-      def executor
-        @executor
-      end
-
+      # @return [Integer]
       def rank
         if @rank.nil?
           @rank = 0
-          queries.each { |q|
+          queries.each do |q|
             @rank += (q.rank + 1)
-          }
+          end
           @rank -= 1000 if verb.nil?
         end
         @rank
@@ -128,10 +129,10 @@ module Gamefic
       def valid? actor, objects
         return false if objects.length != queries.length
         i = 0
-        queries.each { |p|
+        queries.each do |p|
           return false unless p.include?(actor, objects[i])
           i += 1
-        }
+        end
         true
       end
 
@@ -139,11 +140,11 @@ module Gamefic
         i = 0
         result = []
         matches = Gamefic::Query::Matches.new([], '', '')
-        queries.each { |p|
-          return nil if tokens[i].nil? and matches.remaining == ''
+        queries.each do |p|
+          return nil if tokens[i].nil? && matches.remaining == ''
           matches = p.resolve(actor, "#{matches.remaining} #{tokens[i]}".strip, continued: (i < queries.length - 1))
           return nil if matches.objects.empty?
-          accepted = matches.objects.select{|o| p.accept?(o)}
+          accepted = matches.objects.select { |o| p.accept?(o) }
           return nil if accepted.empty?
           if p.ambiguous?
             result.push accepted
@@ -152,19 +153,15 @@ module Gamefic
             result.push accepted.first
           end
           i += 1
-        }
-        self.new(actor, result)
+        end
+        new(actor, result)
       end
 
       protected
 
-      def verb= sym
-        @verb = sym
-      end
+      attr_writer :verb
 
-      def meta= bool
-        @meta = bool
-      end
+      attr_writer :meta
     end
   end
 end
