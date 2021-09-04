8c61d925137d6504dd6d5783ebbd0aefea423d0f
Fred Snyder <fsnyder@castwide.com>
2021-09-04 13:28:37 -0400

    Scene::Base#[] spec

--- spec/scene/base_spec.rb
+++ spec/scene/base_spec.rb
@@ -17,4 +17,9 @@ describe Gamefic::Scene::Base do
     expect(actor.entered?(klass)).to be(true)
     expect(actor.entered?(scene)).to be(true)
   end
+
+  it 'populates the data hash' do
+    scene = Gamefic::Scene::Base.new(nil, foo: 'bar')
+    expect(scene[:foo]).to eq('bar')
+  end
 end
