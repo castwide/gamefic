07062ea0eb57c985ebeca608f1c365b0a223139c
Fred Snyder <fsnyder@castwide.com>
2023-03-21 14:19:14 -0400

    Common logger instance

--- lib/gamefic/logging.rb
+++ lib/gamefic/logging.rb
@@ -8,10 +8,14 @@ module Gamefic
 
     # @return [Logger]
     def logger
-      @logger ||= Logger.new(STDERR, level: Logger::DEBUG)
+      @@logger ||= Logger.new(STDERR, level: Logger::WARN)
                         .tap do |l|
                           l.formatter = proc { |sev, _dt, _prog, msg| "[#{sev}] #{msg}\n"}
                         end
     end
+  end
+
+  def self.logger
+    Logging.logger
   end
 end
