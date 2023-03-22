0ba6c88c57c8a6a1ccea7e883776bd19ae80f5bb
Fred Snyder <fsnyder@castwide.com>
2023-03-22 18:45:05 -0400

    Refactored Describable specs

--- spec/describable_spec.rb
+++ spec/describable_spec.rb
@@ -1,117 +1,105 @@
-require "gamefic"
-
-class Described
-  include Gamefic::Describable
-end
+# frozen_string_literal: true
 
 describe Gamefic::Describable do
+  let(:described) do
+    Object.new.tap { |obj| obj.extend Gamefic::Describable }
+  end
+
   it "determines indefinite article from name" do
-    d = Described.new
-    d.name = "a thing"
-    expect(d.name).to eq("thing")
-    expect(d.indefinite_article).to eq("a")
-    d.name = "an object"
-    expect(d.name).to eq("object")
-    expect(d.indefinite_article).to eq("an")
+    described.name = "a thing"
+    expect(described.name).to eq("thing")
+    expect(described.indefinite_article).to eq("a")
+    described.name = "an object"
+    expect(described.name).to eq("object")
+    expect(described.indefinite_article).to eq("an")
   end
 
   it "automatically uses \"the\" for definite articles" do
-    d = Described.new
-    d.name = "a thing"
-    expect(d.definite_article).to eq("the")
-    d = Described.new
-    d.name = "an object"
-    expect(d.definite_article).to eq("the")
+    described.name = "a thing"
+    expect(described.definite_article).to eq("the")
+    described.name = "an object"
+    expect(described.definite_article).to eq("the")
   end
 
   it "uses correct definite and indefinite articles" do
-    d = Described.new
-    d.name = "a thing"
-    expect(d.definitely).to eq("the thing")
-    expect(d.indefinitely).to eq("a thing")
+    described.name = "a thing"
+    expect(described.definitely).to eq("the thing")
+    expect(described.indefinitely).to eq("a thing")
   end
 
   it "tries to guess indefinite articles" do
-    d = Described.new
-    d.name = "thing"
-    expect(d.indefinite_article).to eq("a")
-    d.name = "object"
-    expect(d.indefinite_article).to eq("an")
+    described.name = "thing"
+    expect(described.indefinite_article).to eq("a")
+    described.name = "object"
+    expect(described.indefinite_article).to eq("an")
   end
 
   it "accepts custom articles" do
-    d = Described.new
     definite = "the bunch of"
     indefinite = "a bunch of"
     name = "grapes"
-    d.name = name
-    d.definite_article = definite
-    d.indefinite_article = indefinite
-    expect(d.definitely).to eq("#{definite} #{name}")
-    expect(d.indefinitely).to eq("#{indefinite} #{name}")
+    described.name = name
+    described.definite_article = definite
+    described.indefinite_article = indefinite
+    expect(described.definitely).to eq("#{definite} #{name}")
+    expect(described.indefinitely).to eq("#{indefinite} #{name}")
   end
 
   it "ignores articles for proper names" do
-    d = Described.new
-    d.name = "John Smith"
-    d.proper_named = true
-    expect(d.definitely).to eq("John Smith")
-    expect(d.indefinitely).to eq("John Smith")
-    d.name = "John Doe"
-    expect(d.definitely).to eq("John Doe")
-    expect(d.indefinitely).to eq("John Doe")
+    described.name = "John Smith"
+    described.proper_named = true
+    expect(described.definitely).to eq("John Smith")
+    expect(described.indefinitely).to eq("John Smith")
+    described.name = "John Doe"
+    expect(described.definitely).to eq("John Doe")
+    expect(described.indefinitely).to eq("John Doe")
   end
 
   it "recognizes proper names starting with \"The\"" do
-    d = Described.new
-    d.proper_named = true
-    d.name = "The Thing"
-    expect(d.definitely).to eq("The Thing")
-    expect(d.indefinitely).to eq("The Thing")
-    d.name = "The Hulk"
-    expect(d.definitely).to eq("The Hulk")
-    expect(d.indefinitely).to eq("The Hulk")
+    described.proper_named = true
+    described.name = "The Thing"
+    expect(described.definitely).to eq("The Thing")
+    expect(described.indefinitely).to eq("The Thing")
+    described.name = "The Hulk"
+    expect(described.definitely).to eq("The Hulk")
+    expect(described.indefinitely).to eq("The Hulk")
   end
 
   it "recognizes proper names starting with \"the\"" do
-    d = Described.new
-    d.proper_named = true
-    d.name = "the Thing"
-    expect(d.definitely).to eq("the Thing")
-    expect(d.indefinitely).to eq("the Thing")
-    d.name = "the Hulk"
-    expect(d.definitely).to eq("the Hulk")
-    expect(d.indefinitely).to eq("the Hulk")
+    described.proper_named = true
+    described.name = "the Thing"
+    expect(described.definitely).to eq("the Thing")
+    expect(described.indefinitely).to eq("the Thing")
+    described.name = "the Hulk"
+    expect(described.definitely).to eq("the Hulk")
+    expect(described.indefinitely).to eq("the Hulk")
   end
 
   it "updates names with definite articles after proper naming" do
-    d = Described.new
-    d.name = "the Thing"
-    d.proper_named = true
-    expect(d.definitely).to eq("the Thing")
+    described.name = "the Thing"
+    described.proper_named = true
+    expect(described.definitely).to eq("the Thing")
   end
 
   it "avoids extraneous spaces for blank articles" do
-    d = Described.new
-    d.name = "thing"
-    d.definite_article = ""
-    d.indefinite_article = ""
-    expect(d.definitely).to eq("thing")
-    expect(d.indefinitely).to eq("thing")
+    described.name = "thing"
+    described.definite_article = ""
+    described.indefinite_article = ""
+    expect(described.definitely).to eq("thing")
+    expect(described.indefinitely).to eq("thing")
   end
 
   it 'tracks descriptions' do
-    d = Described.new
-    d.name = 'thing'
-    expect(d).not_to be_has_description
+    described.name = 'thing'
+    expect(described).not_to be_has_description
     text = 'a described thing'
-    d.description = text
-    expect(d).to be_has_description
-    expect(d.description).to eq(text)
+    described.description = text
+    expect(described).to be_has_description
+    expect(described.description).to eq(text)
   end
 
   # it 'has a default description' do
-  #   d = Described.new
-  #   expect(d.description).to eq(Gamefic::Describable.default_description)
+  #   d = Describedescribed.new
+  #   expect(described.description).to eq(Gamefic::Describable.default_description)
   # end
 end
