53538b8f5333602cc6b1504cac5067c2c649e28d
Fred Snyder <fsnyder@castwide.com>
2023-04-03 23:26:30 -0400

    Subplot snapshot spec

--- spec/gamefic/plot/darkroom_spec.rb
+++ spec/gamefic/plot/darkroom_spec.rb
@@ -1,12 +1,21 @@
 RSpec.describe Gamefic::Plot::Darkroom do
-  it 'saves and restores a snapshot' do
+  it 'saves and restores entities' do
     plot1 = Gamefic::Plot.new
     plot1.make Gamefic::Entity, name: 'thing'
     snapshot = plot1.save
-    expect(snapshot).to be_a(String)
 
     plot2 = Gamefic::Plot.new
     plot2.restore snapshot
     expect(plot2.entities.first.name).to eq('thing')
+  end
+
+  it 'saves and restores subplots' do
+    plot1 = Gamefic::Plot.new
+    plot1.branch Gamefic::Subplot
+    snapshot = plot1.save
+
+    plot2 = Gamefic::Plot.new
+    plot2.restore snapshot
+    expect(plot2.subplots.first).to be_a(Gamefic::Subplot)
   end
 end
