bbaabe26a714185b6beba5da1f3f4e8851090849
Fred Snyder <fsnyder@castwide.com>
2023-04-03 22:36:47 -0400

    Deprecated code

--- lib/gamefic/query.rb
+++ lib/gamefic/query.rb
@@ -3,10 +3,3 @@ require 'gamefic/query/definition'
 require 'gamefic/query/general'
 require 'gamefic/query/relative'
 require 'gamefic/query/textual'
-
-# Steps in parsing a command:
-# * Tokenize from syntaxes
-# * For each argument:
-#     * Filter by scope (available, children, etc.)
-#     * Match objects to tokens
-# * If all arguments have a matching object, we win!
