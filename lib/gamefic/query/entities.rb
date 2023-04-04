7f122d1418098e690030c40806913d5ba86bf66c
Fred Snyder <fsnyder@castwide.com>
2023-04-04 01:10:12 -0400

    Removed unused query nesting

--- lib/gamefic/query/entities.rb
+++ lib/gamefic/query/entities.rb
@@ -3,8 +3,6 @@ module Gamefic
     class Entities
       include Abstract
 
-      NEST_REGEXP = / in | on | of | from | inside | from inside /.freeze
-
       # @param query [Class<Gamefic::Query::Abstract>] Gemeral, Relative, or Textual
       # @param args [Array<Object>]
       def initialize query, *args, ambiguous: false, **opts
@@ -61,28 +59,6 @@ module Gamefic
             arg
           end
         end
-      end
-
-      def nested?(token)
-        !token.match(NEST_REGEXP).nil?
-      end
-
-      def denest(objects, token)
-        parts = token.split(NEST_REGEXP)
-        current = parts.pop
-        last_result = objects.select { |e| e.specified?(current) }
-        last_result = objects.select { |e| e.specified?(current, fuzzy: true) } if last_result.empty?
-        until parts.empty?
-          current = "#{parts.last} #{current}"
-          result = last_result.select { |e| e.specified?(current) }
-          result = last_result.select { |e| e.specified?(current, fuzzy: true) } if result.empty?
-          break if result.empty?
-          parts.pop
-          last_result = result
-        end
-        return [] if last_result.empty? or last_result.length > 1
-        return last_result if parts.empty?
-        denest(last_result[0].children, parts.join(' '))
       end
     end
   end
