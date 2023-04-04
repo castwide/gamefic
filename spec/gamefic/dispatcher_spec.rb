72a6fd175c5eb6c806349c252029e0a81eede32a
Fred Snyder <fsnyder@castwide.com>
2023-04-04 18:43:58 -0400

    Dispatcher spec

--- spec/gamefic/dispatcher_spec.rb
+++ spec/gamefic/dispatcher_spec.rb
@@ -1,3 +1,13 @@
 describe Gamefic::Dispatcher do
-
+  it 'filters and orders actions' do
+    playbook = Gamefic::Playbook.new
+    response1 = playbook.respond(:command) { |_| nil }
+    response2 = playbook.respond(:command) { |_| nil }
+    actor = Gamefic::Actor.new
+    actor.playbooks.push playbook
+    dispatcher = Gamefic::Dispatcher.dispatch(actor, 'command')
+    expect(dispatcher.next.response).to be(response2)
+    expect(dispatcher.next.response).to be(response1)
+    expect(dispatcher.next).to be_nil
+  end
 end
