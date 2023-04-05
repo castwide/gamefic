c7c5f1316fcca91b64b2faea11523fa2249a7c9d
Fred Snyder <fsnyder@castwide.com>
2023-04-05 16:18:29 -0400

    General queries accept procs

--- lib/gamefic/query/scoped.rb
+++ lib/gamefic/query/scoped.rb
@@ -1,12 +1,11 @@
 module Gamefic
   module Query
+    # A Scoped query uses a Scope to select entities to filter based on their
+    # relationship to the entity performing the query. For example,
+    # Scope::Children would filter from an array of the entity's descendants.
+    #
+    # @return [Class<Gamefic::Scope::Base>]
     class Scoped < Base
-      # A Scoped query uses a Scope to select entities to filter in the query
-      # based on their relationship to the entity performing the query. For
-      # example, Scope::Children would filter from an array of the entity's
-      # child entities and the children's accessible descendants.
-      #
-      # @return [Class<Gamefic::Scope::Base>]
       attr_reader :scope
 
       # @param scope [Class<Gamefic::Scope::Base>]
--- spec/gamefic/query/general_spec.rb
+++ spec/gamefic/query/general_spec.rb
@@ -12,4 +12,19 @@ describe Gamefic::Query::General do
     result = general.query(nil, 'three')
     expect(result.match).to be_nil
   end
+
+  it 'accepts procs' do
+    prc = proc { ['one', 'two']}
+    general = Gamefic::Query::General.new(prc)
+    result = general.query(nil, 'one')
+    expect(result.match).to eq('one')
+  end
+
+  it 'accepts procs with an argument' do
+    subject = []
+    prc = proc { |subj| subj + ['one'] }
+    general = Gamefic::Query::General.new(prc)
+    result = general.query(subject, 'one')
+    expect(result.match).to eq('one')
+  end
 end
