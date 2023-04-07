bfa4c23bfc7bd922d27aa946169b29c148057ac2
Fred Snyder <fsnyder@castwide.com>
2023-04-07 16:34:24 -0400

    Proxy documentation

--- lib/gamefic/proxy.rb
+++ lib/gamefic/proxy.rb
@@ -1,4 +1,15 @@
 module Gamefic
+  # An object that acts as a delegate between entity instances in a Plot and
+  # entity references in the plot's theater.
+  #
+  # Proxies allow scripts to retain static references to entities across plot
+  # restorations. When a plot gets restored from a snapshot, the variables in
+  # the script that reference entities should point correctly to the restored
+  # entities.
+  #
+  # Proxies and Entities are designed to be interchangeable. Authors should
+  # not need to care whether a variable points to a Proxy or an Entity.
+  #
   class Proxy
     # @return [Plot, Subplot]
     attr_reader :host
