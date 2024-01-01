4a02f03ae534d21c758f0c37bbf4f13922d1ad82
Fred Snyder <fsnyder@castwide.com>
2024-01-01 08:09:01 -0500

    Spec for included blocks

--- spec/gamefic/scriptable_spec.rb
+++ spec/gamefic/scriptable_spec.rb
@@ -1,23 +1,32 @@
 describe Gamefic::Scriptable do
+  let(:scriptable) { Module.new.extend(Gamefic::Scriptable) }
+
   it 'includes blocks' do
-    mod = Module.new
-    mod.extend Gamefic::Scriptable
-    mod.script do
+    scriptable.script do
       pause :extended_pause
     end
     klass = Class.new(Gamefic::Narrative)
-    klass.include mod
+    klass.include scriptable
     narr = klass.new
     expect(narr.scenes).to include(:extended_pause)
   end
 
   it 'makes attribute seeds' do
-    mod = Module.new
-    mod.extend Gamefic::Scriptable
-    mod.attr_seed(:foo) { make Gamefic::Entity, name: 'foo' }
+    scriptable.attr_seed(:foo) { make Gamefic::Entity, name: 'foo' }
     klass = Class.new(Gamefic::Narrative)
-    klass.include mod
+    klass.include scriptable
     narr = klass.new
     expect(narr.foo).to be_a(Gamefic::Entity)
+  end
+
+  it 'includes module blocks once' do
+    # This test is necessary because Opal can duplicate included modules
+    scriptable.script { @foo = Object.new }
+    other = Module.new.extend(Gamefic::Scriptable)
+    other.include scriptable
+    klass = Class.new(Gamefic::Narrative)
+    klass.include scriptable
+    klass.include other
+    expect(klass.included_blocks).to be_one
   end
 end
