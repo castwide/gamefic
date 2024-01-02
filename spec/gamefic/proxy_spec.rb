3fd64b7b073b6347fa981fa7177916117913e843
Fred Snyder <fsnyder@castwide.com>
2024-01-01 19:41:46 -0500

    Proxy spec

---
+++ spec/gamefic/proxy_spec.rb
@@ -0,0 +1,22 @@
+describe Gamefic::Proxy do
+  let(:plot) do
+    klass = Class.new(Gamefic::Narrative) do
+      attr_reader :foo
+
+      seed { @foo = make Gamefic::Entity, name: 'foo' }
+    end
+    klass.new
+  end
+
+  it 'fetches from instance methods' do
+    proxy = Gamefic::Proxy.new(:foo)
+    object = proxy.fetch(plot)
+    expect(object).to be(plot.foo)
+  end
+
+  it 'fetches from instance variables' do
+    proxy = Gamefic::Proxy.new(:@foo)
+    object = proxy.fetch(plot)
+    expect(object).to be(plot.foo)
+  end
+end
