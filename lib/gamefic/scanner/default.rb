7c3968eb2909ee643483f423b2926ca75c9673e8
Fred Snyder <fsnyder@castwide.com>
2024-09-15 18:04:27 -0400

    Refactor Default and Fuzzy scanners

--- lib/gamefic/scanner/default.rb
+++ lib/gamefic/scanner/default.rb
@@ -18,25 +18,11 @@ module Gamefic
 
       # @return [Result]
       def scan
-        scan_strict
-      end
-
-      def strict
-        scan_strict
-      end
-
-      def fuzzy
-        scan_fuzzy
-      end
-
-      private
-
-      def scan_strict_or_fuzzy method
         words = token.keywords
         available = selection.clone
         filtered = []
         words.each_with_index do |word, idx|
-          tested = send(method, available, word)
+          tested = match_word(available, word)
           return Result.new(selection, token, filtered, words[idx..].join(' ')) if tested.empty?
 
           filtered = tested
@@ -45,33 +31,8 @@ module Gamefic
         Result.new(selection, token, filtered, '')
       end
 
-      # @return [Result]
-      def scan_strict
-        scan_strict_or_fuzzy(:select_strict)
-      end
-
-      # @return [Result]
-      def scan_fuzzy
-        scan_strict_or_fuzzy(:select_fuzzy)
-      end
-
-      # @return [Result]
-      def scan_text
-        case selection
-        when Regexp
-          return Result.new(selection, token, token, '') if token =~ selection
-        else
-          return Result.new(selection, token, selection, token[selection.length..]) if token.start_with?(selection)
-        end
-        Result.new(selection, token, '', token)
-      end
-
-      def select_strict available, word
+      def match_word available, word
         available.select { |obj| obj.keywords.include?(word) }
-      end
-
-      def select_fuzzy available, word
-        available.select { |obj| obj.keywords.any? { |wrd| wrd.start_with?(word) } }
       end
     end
   end
--- lib/gamefic/scanner/fuzzy.rb
+++ lib/gamefic/scanner/fuzzy.rb
@@ -3,9 +3,8 @@
 module Gamefic
   module Scanner
     class Fuzzy < Default
-      # @return [Result]
-      def scan
-        scan_fuzzy
+      def match_word available, word
+        available.select { |obj| obj.keywords.any? { |wrd| wrd.start_with?(word) } }
       end
     end
   end
