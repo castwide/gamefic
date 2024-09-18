d96ed7270af3f5c2f86de819f2e734d2e01de738
Fred Snyder <fsnyder@castwide.com>
2024-09-18 07:41:44 -0400

    Move command composition to dispatcher

--- lib/gamefic/dispatcher.rb
+++ lib/gamefic/dispatcher.rb
@@ -47,7 +47,7 @@ module Gamefic
     # @return [Dispatcher]
     def self.dispatch actor, input
       expressions = Syntax.tokenize(input, actor.epic.syntaxes)
-      command = Composer.compose(actor, expressions)
+      command = compose(actor, expressions)
       new(actor, command)
     end
 
@@ -58,6 +58,29 @@ module Gamefic
     def self.dispatch_from_params actor, verb, params
       command = Command.new(verb, params)
       new(actor, command)
+    end
+
+    class << self
+      private
+
+      # @param actor [Actor]
+      # @param expressions [Array<Expression>]
+      # @return [Command]
+      def compose actor, expressions
+        expressions.flat_map { |expression| expression_to_commands(actor, expression) }
+                   .first || Command.new(nil, [])
+      end
+
+      # @param actor [Actor]
+      # @param expression [Expression]
+      # @return [Array<Command>]
+      def expression_to_commands actor, expression
+        actor.epic
+             .responses_for(expression.verb)
+             .map { |response| response.to_command(actor, expression) }
+             .compact
+             .sort_by.with_index { |result, idx| [-result.precision, -result.strictness, idx] }
+      end
     end
 
     protected
