b4435aa14164a78298021aa0dcdcc657202ee221
Fred Snyder <fsnyder@castwide.com>
2024-10-06 11:29:01 -0400

    Command hooks

--- lib/gamefic/rulebook/hooks.rb
+++ lib/gamefic/rulebook/hooks.rb
@@ -9,9 +9,15 @@ module Gamefic
 
       attr_reader :after_actions
 
+      attr_reader :before_commands
+
+      attr_reader :after_commands
+
       def initialize
         @before_actions = []
         @after_actions = []
+        @before_commands = []
+        @after_commands = []
       end
 
       def freeze
@@ -22,23 +28,35 @@ module Gamefic
       end
 
       def before_action narrative, *verbs, &block
+        Gamefic.logger.warn "#{caller.first ? "#{caller.first}: " : ''}`before_action` is deprecated. Use `before_command` instead."
         before_actions.push Action::Hook.new(verbs, Callback.new(narrative, block))
       end
 
       def after_action narrative, *verbs, &block
+        Gamefic.logger.warn "#{caller.first ? "#{caller.first}: " : ''}`after_action` is deprecated. Use `after_command` instead."
         after_actions.push Action::Hook.new(verbs, Callback.new(narrative, block))
       end
 
+      def before_command narrative, *verbs, &block
+        before_commands.push Action::Hook.new(verbs, Callback.new(narrative, block))
+      end
+
+      def after_command narrative, *verbs, &block
+        after_commands.push Action::Hook.new(verbs, Callback.new(narrative, block))
+      end
+
       def empty?
-        before_actions.empty? && after_actions.empty?
+        [before_actions, after_actions, before_commands, after_commands].all?(&:empty?)
       end
 
       def run_before action
         run_action_hooks action, before_actions
+        run_command_hooks action.actor, before_commands
       end
 
       def run_after action
         run_action_hooks action, after_actions
+        run_command_hooks action.actor, after_commands
       end
 
       private
@@ -50,6 +68,16 @@ module Gamefic
           next unless hook.match?(action.verb)
 
           hook.callback.run action
+        end
+      end
+
+      def run_command_hooks actor, hooks
+        hooks.each do |hook|
+          break if actor.cancelling?
+
+          next unless hook.match?(actor.command.verb)
+
+          hook.callback.run actor, actor.command
         end
       end
     end
