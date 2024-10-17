82c3a6abcf8881b38d90e34f6cf24b30d8f48123
Fred Snyder <fsnyder@castwide.com>
2024-10-17 00:41:43 -0400

    Request from params pads nil results

--- lib/gamefic/response/request.rb
+++ lib/gamefic/response/request.rb
@@ -37,10 +37,13 @@ module Gamefic
       def self.from_params(actor, response, params)
         results = response.queries.zip(params).each_with_object([]) do |parts, matches|
           query, param = parts
-          break matches unless query.accept?(actor, param)
-
-          matches.push Query::Result.new(param, '')
+          if query.accept?(actor, param)
+            matches.push Query::Result.new(param, '')
+          else
+            matches.push nil
+          end
         end
+        (params.length - response.queries.length).times { results.push nil }
         Request.new(response, results)
       end
     end
