0e2057a95d0d9d313d8bf58c2eaf4e2f8c7bcb32
Fred Snyder <fsnyder@castwide.com>
2024-10-20 20:57:09 -0400

    Binding runs code in narrative instance

--- lib/gamefic/binding.rb
+++ lib/gamefic/binding.rb
@@ -7,17 +7,17 @@ module Gamefic
         @registry ||= {}
       end
 
-      def push object, narrative
+      def push(object, narrative)
         registry[object] ||= []
         registry[object].push narrative
       end
 
-      def pop object
+      def pop(object)
         registry[object].pop
         registry.delete(object) if registry[object].empty?
       end
 
-      def for object
+      def for(object)
         registry.fetch(object, []).last
       end
     end
@@ -31,22 +31,10 @@ module Gamefic
 
     def call(*args)
       args.each { |arg| Binding.push arg, @narrative }
-      instance_exec(*args, &@code)
+      @narrative.instance_exec(*args, &@code)
     ensure
       args.each { |arg| Binding.pop arg }
     end
     alias [] call
-
-    def method_missing(symbol, ...)
-      if respond_to_missing?(symbol)
-        @narrative.send(symbol, ...)
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(symbol, _ = false)
-      (@narrative.bound_methods).include?(symbol) || super
-    end
   end
 end
