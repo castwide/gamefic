7d9fc7e8ad4f3b40b0b2e2a3ddba1391f048139d
Fred Snyder <fsnyder@castwide.com>
2024-11-08 01:32:21 -0500

    Scriptable entity class methods

--- lib/gamefic/scriptable/seeds.rb
+++ lib/gamefic/scriptable/seeds.rb
@@ -11,6 +11,59 @@ module Gamefic
         seeds.push(proc { methods.flatten.each { |method| send(method) } }) unless methods.empty?
         seeds.push block if block
       end
+
+      # Construct an entity.
+      #
+      # This method adds an instance method for the entity and a class method to
+      # reference it with a proxy.
+      #
+      # @param name [Symbol, String] The method name for the entity
+      # @param klass [Class<Gamefic::Entity>]
+      # @return [void]
+      def construct name, klass, **opts
+        ivname = "@#{name}"
+        define_method(name) do
+          return instance_variable_get(ivname) if instance_variable_defined?(ivname)
+
+          instance_variable_set(ivname, make(klass, **unproxy(opts)))
+        end
+        seed { send(name) }
+        define_singleton_method(name) { Proxy::Attr.new(name) }
+      end
+      alias attr_make construct
+      alias attr_seed construct
+
+      # Add an entity to be seeded when the narrative gets instantiated.
+      #
+      # @param klass [Class<Gamefic::Entity>]
+      # @return [void]
+      def make klass, **opts
+        seed { make(klass, **unproxy(opts)) }
+      end
+      alias make_seed make
+      alias seed_make make
     end
+
+    # Lazy pick an entity.
+    #
+    # @example
+    #   pick('the red box')
+    #
+    # @param args [Array]
+    # @return [Proxy]
+    def pick *args
+      Proxy::Pick.new(*args)
+    end
+    alias lazy_pick pick
+
+    # Lazy pick an entity or raise an error.
+    #
+    # @note The class method version of `pick!` returns a proxy, so the error
+    #   won't get raised until it gets unproxied in an instance.
+    #
+    def pick! *args
+      Proxy::PickEx.new(*args)
+    end
+    alias lazy_pick! pick!
   end
 end
